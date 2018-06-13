ddff4202c27b1768447d28432659350a73f05918
PantelisElinas <pantelis.elinas@data61.csiro.au>
2018-06-13 13:53:35 +1000

    updated version to 0.1.0rc in preperation for 0.1.0 release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='stellar-ml',
-      version='0.1.0b',
+      version='0.1.0.rcb',
       description='Stellar Machine Learning Library',
       license='Apache 2.0',
       install_requires=['keras', 'numpy'],
