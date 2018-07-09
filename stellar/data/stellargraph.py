4f75cef81e8cd7853ce1aa0449805b12bee602c1
Andrew Docherty <docherty@gmail.com>
2018-07-09 17:40:38 +1000

    Updated graph schema with node and edge type maps

--- stellar/data/stellargraph.py
+++ stellar/data/stellargraph.py
@@ -17,11 +17,17 @@ import queue
 from networkx.classes.multigraph import MultiGraph
 from networkx.classes.multidigraph import MultiDiGraph
 
+from collections import namedtuple
+
+EdgeType = namedtuple("EdgeType", "n1 rel n2")
+
 
 class GraphSchema:
     node_types = None
     edge_types = None
     schema = None
+    node_type_map = None
+    edge_type_map = None
 
     def node_key_to_index(self, key):
         try:
@@ -63,80 +69,136 @@ class GraphSchema:
                 s += "   {} -- {} -> {}\n".format(*e)
         return s
 
-    def create_type_tree(self, head_node_types, levels):
+    def node_type(self, node, index=False):
+        """
+        Return the type of the node
+        Args:
+            node: The node ID from the original graph
+            index: Return a numeric type index if True,
+                otherwise return the type name.
+
+        Returns:
+            A node type name or index
+        """
+        try:
+            nt = self.node_type_map[node]
+            node_type = nt if index else self.node_types[index]
+
+        except:
+            print("Warning: Node '{}' not found in type map.".format(node))
+            node_type = None
+        return node_type
+
+    def edge_type(self, edge, index=False):
+        """
+        Return the type of the edge
+        Args:
+            edge: The edge ID from the original graph [a tuple (n1,n2)]
+            index: Return a numeric type index if True,
+                otherwise return the type triple:
+                 (source_node_type, relation_type, dest_node_type).
+
+        Returns:
+            A node type triple or index
+        """
+        try:
+            if edge in self.edge_type_map:
+                et = self.edge_type_map[edge]
+            else:
+                et = self.edge_type_map[(edge[1], edge[0])]
+            edge_type = et if index else self.edge_types[et]
+
+        except:
+            print("Warning: Edge '{}' not found in type map.".format(edge))
+            edge_type = None
+        return edge_type
+
+    def edge_types_for_node(self, node_type):
+        try:
+            edge_types = self.schema[node_type]
+        except:
+            print("Warning: Node type '{}' not found.".format(node_type))
+            edge_types = []
+        return edge_types
+
+    def create_sampling_tree(self, head_node_types, n_hops):
+        """
+        Returns a sampling tree for the specified head node types
+        for neighbours up to n_hops away.
+
+        Args:
+            head_node_types: An iterable of the types of the head nodes
+            n_hops: The number of hops away
+
+        Returns:
+
+        """
+
         def gen_key(key, *args):
             return key + "_".join(map(str, args))
 
-        def get_type_tree(head_nodes, level):
-            """
-            Build tree in list of lists format
-            Each node is a tuple of:
-            (node_key, node_type, [neighbours])
+        def get_neighbor_types(node_type, level, key=""):
+            if level == 0:
+                return []
 
-            Args:
-                head_nodes:
-                level:
-
-            Returns:
-                List of form [ (node_key, node_type, [neighbours]), ...]
-            """
-            def get_neighbor_types(node_type, level, key=""):
-                if level == 0:
-                    return []
-
-                neighbour_node_types = [
-                    (
-                        gen_key(key, ii),
-                        et[2],
-                        get_neighbor_types(et[2], level - 1, gen_key(key, ii) + "_"),
-                    )
-                    for ii, et in enumerate(self.schema[node_type])
-                ]
-                return neighbour_node_types
-
-            # Create root nodes at top of heirachy and recurse in schema from head nodes.
             neighbour_node_types = [
                 (
-                    str(jj),
-                    node_type,
-                    get_neighbor_types(node_type, level, gen_key("", jj) + "#"),
+                    gen_key(key, ii),
+                    et,
+                    get_neighbor_types(et.n2, level - 1, gen_key(key, ii) + "_"),
                 )
-                for jj, node_type in enumerate(head_nodes)
+                for ii, et in enumerate(self.schema[node_type])
             ]
             return neighbour_node_types
 
-        def tree_to_list(tree):
-            """
-            Convert tree in triple-list format to a type list for HinSAGE
-            Args:
-                tree:
-            Returns:
-                List of form [(node_type, [decendents]), ...]
-            """
-            # Create adjacency list using BFS
-            to_process = queue.Queue()
-            key_dict = dict()
-            clist = list()
-            cinx = 0
-            to_process.put(tree)
-            while not to_process.empty():
-                subtree = to_process.get()
+        # Create root nodes at top of heirachy and recurse in schema from head nodes.
+        neighbour_node_types = [
+            (
+                str(jj),
+                node_type,
+                get_neighbor_types(node_type, n_hops, gen_key("", jj) + "#"),
+            )
+            for jj, node_type in enumerate(head_node_types)
+        ]
+        return neighbour_node_types
 
-                for node in subtree:
-                    key, node_type, dtree = node
-                    neighbours = [c[0] for c in dtree]
-                    key_dict[key] = len(clist)
+    def create_type_tree(self, head_node_types, n_hops):
+        """
+        Creates a BFS sampling tree in list of lists format from head node types.
+        Each node is a tuple of:
+        (node_type, [children])
 
-                    clist.append((node_type, neighbours))
-                    cinx += 1
-                    if len(dtree) > 0:
-                        to_process.put(dtree)
+        Args:
+            head_node_types: Node types of head nodes.
+            n_hops: How many hops to sample.
 
-            # replace keys with indices
-            out_list = [(adj[0], [key_dict[x] for x in adj[1]]) for adj in clist]
-            return out_list
+        Returns:
+            List of form [ (node_type, [children]), ...]
+        """
+        to_process = queue.Queue()
 
-        return tree_to_list(get_type_tree(head_node_types, levels))
+        # Add head nodes
+        clist = list()
+        for ii, hn in enumerate(head_node_types):
+            to_process.put((hn, ii, 0))
+            clist.append((hn, []))
+
+        while not to_process.empty():
+            # Get node, node index, and level
+            nt, ninx, lvl = to_process.get()
+
+            # The ordered list of edge types from this node type
+            ets = self.schema[nt]
+
+            # Iterate over edge types (in order)
+            for et in ets:
+                cinx = len(clist)
+                clist.append((et.n2, []))
+                clist[ninx][1].append(cinx)
+                if lvl < n_hops:
+                    to_process.put((et.n2, cinx, lvl + 1))
+
+        return clist
 
 
 class StellarGraphBase:
@@ -144,10 +206,10 @@ class StellarGraphBase:
         super().__init__(incoming_graph_data, **attr)
 
         # Names of attributes that store the type of nodes and edges
-        self._node_type_attr = "label"
-        self._edge_type_attr = "label"
+        self._node_type_attr = attr.get("node_type_key", "label")
+        self._edge_type_attr = attr.get("edge_type_key", "label")
 
-    def create_graph_schema(self):
+    def create_graph_schema(self, create_type_maps=True):
         """
         Create graph schema in dict of dict format from current graph
         Returns:
@@ -168,29 +230,56 @@ class StellarGraphBase:
             edge_type = edata[self._edge_type_attr]
 
             # Add edge type to node_type_1 data
-            edge_type_tri = (node_type_1, edge_type, node_type_2)
+            edge_type_tri = EdgeType(node_type_1, edge_type, node_type_2)
             edge_types.add(edge_type_tri)
             graph_schema[node_type_1].add(edge_type_tri)
 
             # Also add type to node_2 data if not digraph
             if not self.is_directed():
-                edge_type_tri = (node_type_2, edge_type, node_type_1)
+                edge_type_tri = EdgeType(node_type_2, edge_type, node_type_1)
                 edge_types.add(edge_type_tri)
                 graph_schema[node_type_2].add(edge_type_tri)
 
-        # Create schema object
-        gs = GraphSchema()
-        gs.edge_types = sorted(edge_types)
-        gs.node_types = node_types
+        # Create ordered list of edge_types
+        edge_types =  sorted(edge_types)
 
         # Create keys for node and edge types
-        gs.schema = {
+        schema = {
             node_label: [
-                gs.edge_types[einx]
-                for einx in sorted([gs.edge_types.index(et) for et in list(node_data)])
+                edge_types[einx]
+                for einx in sorted([edge_types.index(et) for et in list(node_data)])
             ]
             for node_label, node_data in graph_schema.items()
         }
+
+        # Create schema object
+        gs = GraphSchema()
+        gs.edge_types = edge_types
+        gs.node_types = node_types
+        gs.schema = schema
+
+        # Create quick type lookups for nodes and edges.
+        # Note: we encode the type index, in the assumption it will take
+        # less storage.
+        if create_type_maps:
+            node_type_map = {
+                n: node_types.index(ndata[self._node_type_attr])
+                for n, ndata in self.nodes(data=True)
+            }
+            edge_type_map = {
+                (n1, n2): edge_types.index(
+                    EdgeType(
+                        node_types[node_type_map[n1]],
+                        edata[self._edge_type_attr],
+                        node_types[node_type_map[n2]],
+                    )
+                )
+                for n1, n2, edata in self.edges(data=True)
+            }
+
+            gs.node_type_map = node_type_map
+            gs.edge_type_map = edge_type_map
+
         return gs
 
 
@@ -199,6 +288,7 @@ class StellarGraph(StellarGraphBase, MultiGraph):
     Our own class for heterogeneous undirected graphs, inherited from nx.MultiGraph,
     with extra stuff to be added that's needed by samplers and mappers
     """
+
     def __init__(self, incoming_graph_data=None, **attr):
         super().__init__(incoming_graph_data, **attr)
 
@@ -208,5 +298,6 @@ class StellarDiGraph(StellarGraphBase, MultiDiGraph):
     Our own class for heterogeneous directed graphs, inherited from nx.MultiDiGraph,
     with extra stuff to be added that's needed by samplers and mappers
     """
+
     def __init__(self, incoming_graph_data=None, **attr):
         super().__init__(incoming_graph_data, **attr)
