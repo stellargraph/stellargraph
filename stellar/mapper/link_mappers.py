Fixed incorrect self.feature_size_by_type calculation

stellar/mapper/link_mappers.py
@@ -296,7 +296,8 @@ class HinSAGELinkMapper(Sequence):
             self.feature_size_by_type = {}
             for nt in self.schema.node_types:
                 feature_sizes = {
-                    np.size(vdata["feature"]) if "feature" in vdata else None
+                    # np.size(vdata["feature"]) if "feature" in vdata else None    # YT: this was a bug! We need np.shape(vdata["feature"])[1] instead
+                    np.shape(vdata["feature"])[1] if "feature" in vdata else None
                     for v, vdata in self.g.nodes(data=True)
                     if self.schema.get_node_type(v) == nt
                 }