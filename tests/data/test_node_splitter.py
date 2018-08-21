dcdb6b1e44a941e0850b5ca83402fa26705362f8
PantelisElinas <pantelis.elinas@data61.csiro.au>
2018-08-21 13:51:14 +1000

    removed write_data* methods and corresponding unit tests

--- tests/data/test_node_splitter.py
+++ tests/data/test_node_splitter.py
@@ -241,57 +241,6 @@ class TestEPGMIOHeterogeneous(unittest.TestCase):
             ),
         )
 
-    def test_write_data_epgm(self):
-
-        nc = 10
-        test_size = 100
-
-        y = self.ds_obj.load_data(
-            self.input_dir,
-            dataset_name=self.dataset_name,
-            node_type=self.node_type,
-            target_attribute=self.target_attribute,
-        )
-
-        y_train, y_val, y_test, y_unlabeled = self.ds_obj.train_test_split(
-            y=y, p=nc, test_size=test_size
-        )
-
-        self.ds_obj.write_data(
-            self.output_dir,
-            dataset_name=self.dataset_name,
-            y_train=y_train,
-            y_test=y_test,
-            y_val=y_val,
-            y_unlabeled=y_unlabeled,
-        )
-
-        # 3 files with json extension should be written in the output directory
-        # The files are edges.json, graphs.json, vertices.json
-        files_in_dir = os.listdir(self.output_dir)
-
-        self.assertEqual(
-            len(files_in_dir),
-            3,
-            "Incorrect number of .json files {:d} vs expected {:d}".format(
-                len(files_in_dir), 3
-            ),
-        )
-
-        self.assertTrue("edges.json" in files_in_dir, "Missing edges.json")
-        self.assertTrue("vertices.json" in files_in_dir, "Missing vertices.json")
-        self.assertTrue("graphs.json" in files_in_dir, "Missing graphs.json")
-
-        # Load the EPGM and check it
-        g_epgm = EPGM(self.output_dir)
-        graphs = g_epgm.G["graphs"]
-
-        self.assertEqual(
-            len(graphs),
-            5,
-            "Incorrect number of graphs {:d} vs expected {:d}".format(len(graphs), 5),
-        )
-
 
 class TestEPGMIOHomogenous(unittest.TestCase):
     def setUp(self):
@@ -337,6 +286,7 @@ class TestEPGMIOHomogenous(unittest.TestCase):
     def tearDown(self):
         # delete the contents of the output directories
         rmtree(self.base_output_directory)
+
 
     def create_toy_dataset(self):
         # 100 node ids with 40 class 0, 40 class 1, and 20 unknown '-1'
@@ -670,57 +620,6 @@ class TestEPGMIOHomogenous(unittest.TestCase):
             ),
         )
 
-    def test_write_data_lab(self):
-
-        nc = 20
-        test_size = 100
-
-        y = self.ds_obj.load_data(
-            self.input_lab,
-            dataset_name=self.dataset_name,
-            node_type=self.node_type,
-            target_attribute=self.target_attribute,
-        )
-
-        y_train, y_val, y_test, y_unlabeled = self.ds_obj.train_test_split(
-            y=y, p=nc, test_size=test_size
-        )
-
-        self.ds_obj.write_data(
-            self.output_dir_lab,
-            dataset_name=self.dataset_name,
-            y_train=y_train,
-            y_test=y_test,
-            y_val=y_val,
-            y_unlabeled=y_unlabeled,
-        )
-
-        # 3 files with json extension should be written in the output directory
-        # The files are edges.json, graphs.json, vertices.json
-        files_in_dir = os.listdir(self.output_dir_lab)
-
-        self.assertEqual(
-            len(files_in_dir),
-            4,
-            "Incorrect number of .idx.* files {:d} vs expected {:d}".format(
-                len(files_in_dir), 4
-            ),
-        )
-
-        self.assertTrue(
-            self.dataset_name + ".idx.train" in files_in_dir, "Missing .idx.train"
-        )
-        self.assertTrue(
-            self.dataset_name + ".idx.test" in files_in_dir, "Missing .idx.test"
-        )
-        self.assertTrue(
-            self.dataset_name + ".idx.validation" in files_in_dir,
-            "Missing .idx.validation",
-        )
-        self.assertTrue(
-            self.dataset_name + ".idx.unlabeled" in files_in_dir,
-            "Missing .idx.unlabeled",
-        )
 
     # Testing with data I/O in EPGM format
     def test_load_epgm(self):
@@ -872,57 +771,6 @@ class TestEPGMIOHomogenous(unittest.TestCase):
             "Test dataset has wrong size {:d} vs expected {:d}".format(
                 self.y_test.shape[0], test_size
             ),
-        )
-
-    def test_write_data_epgm(self):
-
-        nc = 20
-        test_size = 1000
-
-        y = self.ds_obj.load_data(
-            self.input_dir,
-            dataset_name=self.dataset_name,
-            node_type=self.node_type,
-            target_attribute=self.target_attribute,
-        )
-
-        y_train, y_val, y_test, y_unlabeled = self.ds_obj.train_test_split(
-            y=y, p=nc, test_size=test_size
-        )
-
-        self.ds_obj.write_data(
-            self.output_dir,
-            dataset_name=self.dataset_name,
-            y_train=y_train,
-            y_test=y_test,
-            y_val=y_val,
-            y_unlabeled=y_unlabeled,
-        )
-
-        # 3 files with json extension should be written in the output directory
-        # The files are edges.json, graphs.json, vertices.json
-        files_in_dir = os.listdir(self.output_dir)
-
-        self.assertEqual(
-            len(files_in_dir),
-            3,
-            "Incorrect number of .json files {:d} vs expected {:d}".format(
-                len(files_in_dir), 3
-            ),
-        )
-
-        self.assertTrue("edges.json" in files_in_dir, "Missing edges.json")
-        self.assertTrue("vertices.json" in files_in_dir, "Missing vertices.json")
-        self.assertTrue("graphs.json" in files_in_dir, "Missing graphs.json")
-
-        # Load the EPGM and check it
-        g_epgm = EPGM(self.output_dir)
-        graphs = g_epgm.G["graphs"]
-
-        self.assertEqual(
-            len(graphs),
-            5,
-            "Incorrect number of graphs {:d} vs expected {:d}".format(len(graphs), 5),
         )
 
 
