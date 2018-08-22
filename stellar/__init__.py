f949ef9ac7079eb785e8d1cf8544d3082275e40f
Andrew Docherty <docherty@gmail.com>
2018-08-22 16:41:54 +1000

    Moved globals to globals.py from __init__.py

--- stellar/__init__.py
+++ stellar/__init__.py
@@ -3,10 +3,4 @@ Stellar Machine Learning Library
 
 """
 
-
-class GLOBALS:
-    # This is the default for
-    FEATURE_ATTR_NAME = "feature"
-    TARGET_ATTR_NAME = "target"
-    TYPE_ATTR_NAME = "label"
-    UNKNOWN_TARGET_ATTRIBUTE = "-1"
+__all__ = ["data", "layer", "mapper"]
