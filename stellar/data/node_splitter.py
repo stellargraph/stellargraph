c469e0e193e3d52c383681ada4f11967fe3402e9
PantelisElinas <pantelis.elinas@data61.csiro.au>
2018-08-22 10:47:13 +1000

    fixed text formatting

--- stellar/data/node_splitter.py
+++ stellar/data/node_splitter.py
@@ -198,9 +198,7 @@ class NodeSplitter(object):
 
         """
         if type(y) is not np.ndarray:
-            raise ValueError(
-                "({}) y should be numpy array".format(type(self).__name__)
-            )
+            raise ValueError("({}) y should be numpy array".format(type(self).__name__))
         if method != "count" and method != "percent" and method != "absolute":
             raise ValueError(
                 "({}) Valid methods are 'count', 'percent', and 'absolute' not {}".format(
