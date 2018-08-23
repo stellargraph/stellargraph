9e7739485ddff57807c5ba830e29af7320734555
PantelisElinas <pantelis.elinas@data61.csiro.au>
2018-08-23 16:12:15 +1000

    introduced previously removed test

--- tests/data/test_epgm.py
+++ tests/data/test_epgm.py
@@ -131,7 +131,7 @@ class Test_EPGM_IO_Heterogeneous(object):
         # check that G_epgm.G['graphs] has at least one graph head:
         assert len(G_epgm.G["graphs"]) > 0
 
-        # yelp nodes of self.node_type type should have a self.target_attribute attribute
+        # graph nodes of self.node_type type should have a self.target_attribute attribute
         graph_id = G_epgm.G["graphs"][0]["id"]
         assert self.target_attribute in G_epgm.node_attributes(graph_id, self.node_type)
 
@@ -140,35 +140,25 @@ class Test_EPGM_IO_Heterogeneous(object):
         nodes = G_epgm.G["vertices"]
         assert len(nodes) == n_nodes
 
-        # yelp 'user' nodes should have 2 unique values for 'elite' attribute:
+        # 'user' nodes should have 3 unique values for 'elite' attribute:
         # first make sure that all nodes have 'data' key
         assert sum(["data" in v for v in nodes]) == n_nodes
         labels_all = [v["data"].get(self.target_attribute) for v in nodes]
         labels = list(filter(lambda l: l is not None, labels_all))
-        labels_none = list(filter(lambda l: l is None, labels_all))
         assert len(np.unique(labels)) == 3
-        # assert (
-        #     len(labels_none) == 62
-        # ), "yelp contains 52 'user' nodes with undefined '{}' attribute; {} such nodes found instead".format(
-        #     self.target_attribute, len(labels_none)
-        # )
 
     def test_node_types(self):
         """Test the .node_types() method"""
         G_epgm = EPGM(self.input_dir)
         graph_id = G_epgm.G["graphs"][0]["id"]
 
-        # yelp has multiple node types:
+        # dataset has multiple node types:
         node_types = G_epgm.node_types(graph_id)
 
         assert len(node_types) == 3
         assert "user" in node_types
         assert "paper" in node_types
         assert "venue" in node_types
-        # assert "business" in node_types
-        # assert "review" in node_types
-        # assert "city" in node_types
-        # assert "state" in node_types
 
         with pytest.raises(Exception):
             G_epgm.node_types("invalid_graph_id")
@@ -178,15 +168,15 @@ class Test_EPGM_IO_Heterogeneous(object):
         G_epgm = EPGM(self.input_dir)
         graph_id = G_epgm.G["graphs"][0]["id"]
 
-        # yelp has 19 unique 'user' node attributes, including 'elite'
+        # dataset has 1 unique 'user' node attribute, 'elite'
         node_attributes = G_epgm.node_attributes(graph_id, self.node_type)
 
         assert self.target_attribute in node_attributes
-        # assert (
-        #     len(node_attributes) == 19
-        # ), "There should be 19 unique node attributes; found {}".format(
-        #     len(node_attributes)
-        # )
+        assert (
+            len(node_attributes) == 1
+        ), "There should be 1 unique node attribute; found {}".format(
+            len(node_attributes)
+        )
 
         # passing a non-existent node type should return an empty array of node attributes:
         assert len(G_epgm.node_attributes(graph_id, "business")) == 0
