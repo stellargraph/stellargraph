de3a3e9069f79a67e40027d33a543c8499d4f7eb
PantelisElinas <pantelis.elinas@data61.csiro.au>
2018-09-12 09:17:46 +1000

    removed unused module

--- stellargraph/rl/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-The rl package contains classes and functions to do representation learning. The representation learner may be some
-combination of graph ML layers defined in the layer package, with a wrapper specifically designed to produce embeddings.
-
-"""
