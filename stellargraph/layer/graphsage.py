1f30e0db6e5d063ba9b9549841d461d81050384f
Yuriy Tyshetskiy <yuriy.tyshetskiy@data61.csiro.au>
2019-01-03 12:27:39 +1100

    Updated tests for graphsage and hinsage layers to include normalize=None in the arguments.

--- stellargraph/layer/graphsage.py
+++ stellargraph/layer/graphsage.py
@@ -471,11 +471,11 @@ class GraphSAGE:
         if normalize == "l2":
             self._normalization = Lambda(lambda x: K.l2_normalize(x, axis=2))
 
-        elif normalize is None or normalize == "none":
+        elif normalize is None or normalize == "none" or normalize == "None":
             self._normalization = Lambda(lambda x: x)
 
         else:
-            raise ValueError("Normalization should be either 'l2' or 'none'")
+            raise ValueError("Normalization should be either 'l2' or 'none'; received '{}'".format(normalize))
 
         # Get the input_dim and num_samples from the mapper if it is given
         # Use both the schema and head node type from the mapper
