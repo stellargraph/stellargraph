556f9e79182d8a5ef6afd23d4a35fb5763163235
Yuriy Tyshetskiy <yuriy.tyshetskiy@data61.csiro.au>
2019-01-15 12:52:44 +1100

    improved readability of FullBatchNodeGenerator.flow() method

--- stellargraph/mapper/node_mappers.py
+++ stellargraph/mapper/node_mappers.py
@@ -503,11 +503,22 @@ class FullBatchNodeGenerator:
         node_mask = np.zeros(len(self.node_list), dtype=int)
         node_mask[node_indices] = 1
         node_mask = np.ma.make_mask(node_mask)
-        # reorder targets to match the node_mask order, since node_mask effectively sorts node_indices
-        targets = targets[np.argsort(node_indices)]
-        # Reshape y_train to (number of nodes in self.graph, number of classes)
-        N = self.Aadj.shape[0]
-        C = targets.shape[1]
-        targets_rs = np.zeros((N, C))
-        targets_rs[node_mask] = targets
-        return FullBatchNodeSequence(self.features, self.Aadj, node_mask, targets_rs)
+        # # reorder targets to match the node_mask order, since node_mask effectively sorts node_indices
+        # targets = targets[np.argsort(node_indices)]
+        # # Reshape targets to (number of nodes in self.graph, number of classes)
+        # N = self.Aadj.shape[0]
+        # C = targets.shape[1]
+        # y = np.zeros((N, C))
+        # y[node_mask] = targets
+
+        # Reshape targets to (number of nodes in self.graph, number of classes)
+        if targets is not None:
+            N = self.Aadj.shape[0]
+            C = targets.shape[1]
+            y = np.zeros((N, C))
+            for i, t in zip(node_indices, targets):
+                y[i] = t
+        else:
+            y = None
+
+        return FullBatchNodeSequence(self.features, self.Aadj, node_mask, y)
