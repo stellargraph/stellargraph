0148fd58669d3f7e8ef19de3d7c68bac14482155
TheDen <deniskhoshaba@gmail.com>
2019-01-16 15:03:27 +1100

    limit to develop and master

--- .buildkite/script.sh
+++ .buildkite/script.sh
@@ -2,9 +2,14 @@
 
 set -xeo pipefail
 
+upload_tests() {
+  buildkite-agent artifact upload "${BUILDKITE_BUILD_NUMBER}.xml" s3://stellargraph-logs-hosted/pytest/${BUILDKITE_BRANCH}/${BUILDKITE_BUILD_NUMBER}
+}
+
 pip install -q --no-cache-dir -r requirements.txt -e .
 py.test -ra --cov=stellargraph tests/ --doctest-modules --doctest-modules --cov-report=term-missing -p no:cacheprovider --junitxml=./${BUILDKITE_BUILD_NUMBER}.xml
 coveralls
 
-# Upload junitxml to s3
-buildkite-agent artifact upload "${BUILDKITE_BUILD_NUMBER}.xml" s3://stellargraph-logs-hosted/pytest/${BUILDKITE_BUILD_NUMBER}
+if [ "${BUILDKITE_BRANCH}" = "develop"  ] || [ "${BUILDKITE_BRANCH}" = "master" ] ; then
+	upload_tests
+fi
