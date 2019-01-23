90e1153640334131cf02baeec5696f57715dac67
Jeff Wang <wangzhen263@gmail.com>
2019-01-23 13:53:36 +1100

    Added GCN tests

--- stellargraph/mapper/gcn_mappers.py
+++ stellargraph/mapper/gcn_mappers.py
@@ -7,6 +7,7 @@ import networkx as nx
 from ..core.utils import is_real_iterable
 from ..core.graph import StellarGraphBase, GraphSchema
 
+
 def normalize_adj(adj, symmetric=True):
     if symmetric:
         d = sp.diags(np.power(np.array(adj.sum(1)), -0.5).flatten(), 0)
@@ -16,26 +17,32 @@ def normalize_adj(adj, symmetric=True):
         a_norm = d.dot(adj).tocsr()
     return a_norm
 
+
 def preprocess_adj(adj, symmetric=True):
     adj = adj + sp.eye(adj.shape[0])
     adj = normalize_adj(adj, symmetric)
     return adj
+
 
 def normalized_laplacian(adj, symmetric=True):
     adj_normalized = normalize_adj(adj, symmetric)
     laplacian = sp.eye(adj.shape[0]) - adj_normalized
     return laplacian
 
+
 def rescale_laplacian(laplacian):
     try:
-        print('Calculating largest eigenvalue of normalized graph Laplacian...')
-        largest_eigval = eigsh(laplacian, 1, which='LM', return_eigenvectors=False)[0]
+        print("Calculating largest eigenvalue of normalized graph Laplacian...")
+        largest_eigval = eigsh(laplacian, 1, which="LM", return_eigenvectors=False)[0]
     except ArpackNoConvergence:
-        print('Eigenvalue calculation did not converge! Using largest_eigval=2 instead.')
+        print(
+            "Eigenvalue calculation did not converge! Using largest_eigval=2 instead."
+        )
         largest_eigval = 2
 
-    scaled_laplacian = (2. / largest_eigval) * laplacian - sp.eye(laplacian.shape[0])
+    scaled_laplacian = (2.0 / largest_eigval) * laplacian - sp.eye(laplacian.shape[0])
     return scaled_laplacian
+
 
 def chebyshev_polynomial(X, k):
     """Calculate Chebyshev polynomials up to order k. Return a list of sparse matrices."""
@@ -49,25 +56,26 @@ def chebyshev_polynomial(X, k):
         X_ = sp.csr_matrix(X, copy=True)
         return 2 * X_.dot(T_k_minus_one) - T_k_minus_two
 
-    for i in range(2, k+1):
+    for i in range(2, k + 1):
         T_k.append(chebyshev_recurrence(T_k[-1], T_k[-2], X))
 
     return T_k
 
+
 def GCN_A_feats(features, A, **kwargs):
     # build symmetric adjacency matrix
     A = A + A.T.multiply(A.T > A) - A.multiply(A.T > A)
-    filter = kwargs.get('filter', 'localpool')
+    filter = kwargs.get("filter", "localpool")
 
-    if filter == 'localpool':
+    if filter == "localpool":
         """ Local pooling filters (see 'renormalization trick' in Kipf & Welling, arXiv 2016) """
-        print('Using local pooling filters...')
+        print("Using local pooling filters...")
         A = preprocess_adj(A)
-    elif filter == 'chebyshev':
+    elif filter == "chebyshev":
         """ Chebyshev polynomial basis filters (Defferard et al., NIPS 2016)  """
-        print('Using Chebyshev polynomial basis filters...')
+        print("Using Chebyshev polynomial basis filters...")
         T_k = chebyshev_polynomial(rescale_laplacian(normalized_laplacian(A)), 2)
-        features = [features]+T_k
+        features = [features] + T_k
     return features, A
 
 
@@ -88,6 +96,7 @@ class FullBatchNodeSequence(Sequence):
     def __getitem__(self, index):
         return [self.features, self.A], self.targets, self.sample_weight
 
+
 class FullBatchNodeGenerator:
     def __init__(self, G, name=None, func_A_feats=None, **kwargs):
         if not isinstance(G, StellarGraphBase):
@@ -103,7 +112,9 @@ class FullBatchNodeGenerator:
         self.A = nx.adjacency_matrix(G, nodelist=self.nodes)
 
         if func_A_feats:
-            self.features, self.A = func_A_feats(features=self.features, A=self.A, kwargs=kwargs)
+            self.features, self.A = func_A_feats(
+                features=self.features, A=self.A, kwargs=kwargs
+            )
 
         self.kwargs = kwargs
 
@@ -114,7 +125,7 @@ class FullBatchNodeGenerator:
         node_mask = np.array(node_mask, dtype=np.bool)
 
         if targets is not None:
-            y = np.zeros( (len(self.nodes),targets.shape[1]), dtype=np.int32)
+            y = np.zeros((len(self.nodes), targets.shape[1]), dtype=np.int32)
             for idx, t in zip(node_indices, targets):
                 y[idx] = t
 
