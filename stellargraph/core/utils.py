fd4551cab9675c37abe7d705c829707b7017d6fe
Jeff Wang <wangzhen263@gmail.com>
2019-01-25 10:32:40 +1100

    Unify GCN/GAT FullBatchNodeGenerator and mv GCN_Aadj_feats_op and related funcs to utils

--- stellargraph/core/utils.py
+++ stellargraph/core/utils.py
@@ -27,3 +27,72 @@ def is_real_iterable(x):
         True if x is an iterable (but not a string) and False otherwise
     """
     return isinstance(x, collections.Iterable) and not isinstance(x, (str, bytes))
+
+
+def normalized_laplacian(adj, symmetric=True):
+    adj_normalized = normalize_adj(adj, symmetric)
+    laplacian = sp.eye(adj.shape[0]) - adj_normalized
+    return laplacian
+
+
+def rescale_laplacian(laplacian):
+    try:
+        print("Calculating largest eigenvalue of normalized graph Laplacian...")
+        largest_eigval = eigsh(laplacian, 1, which="LM", return_eigenvectors=False)[0]
+    except ArpackNoConvergence:
+        print(
+            "Eigenvalue calculation did not converge! Using largest_eigval=2 instead."
+        )
+        largest_eigval = 2
+
+    scaled_laplacian = (2.0 / largest_eigval) * laplacian - sp.eye(laplacian.shape[0])
+    return scaled_laplacian
+
+
+def chebyshev_polynomial(X, k):
+    """Calculate Chebyshev polynomials up to order k. Return a list of sparse matrices."""
+    print("Calculating Chebyshev polynomials up to order {}...".format(k))
+
+    T_k = list()
+    T_k.append(sp.eye(X.shape[0]).tocsr())
+    T_k.append(X)
+
+    def chebyshev_recurrence(T_k_minus_one, T_k_minus_two, X):
+        X_ = sp.csr_matrix(X, copy=True)
+        return 2 * X_.dot(T_k_minus_one) - T_k_minus_two
+
+    for i in range(2, k + 1):
+        T_k.append(chebyshev_recurrence(T_k[-1], T_k[-2], X))
+
+    return T_k
+
+
+def GCN_Aadj_feats_op(features, A, **kwargs):
+    def normalize_adj(adj, symmetric=True):
+        if symmetric:
+            d = sp.diags(np.power(np.array(adj.sum(1)), -0.5).flatten(), 0)
+            a_norm = adj.dot(d).transpose().dot(d).tocsr()
+        else:
+            d = sp.diags(np.power(np.array(adj.sum(1)), -1).flatten(), 0)
+            a_norm = d.dot(adj).tocsr()
+        return a_norm
+
+    def preprocess_adj(adj, symmetric=True):
+        adj = adj + sp.eye(adj.shape[0])
+        adj = normalize_adj(adj, symmetric)
+        return adj
+
+    # build symmetric adjacency matrix
+    A = A + A.T.multiply(A.T > A) - A.multiply(A.T > A)
+    filter = kwargs.get("filter", "localpool")
+
+    if filter == "localpool":
+        """ Local pooling filters (see 'renormalization trick' in Kipf & Welling, arXiv 2016) """
+        print("Using local pooling filters...")
+        A = preprocess_adj(A)
+    elif filter == "chebyshev":
+        """ Chebyshev polynomial basis filters (Defferard et al., NIPS 2016)  """
+        print("Using Chebyshev polynomial basis filters...")
+        T_k = chebyshev_polynomial(rescale_laplacian(normalized_laplacian(A)), 2)
+        features = [features] + T_k
+    return features, A
