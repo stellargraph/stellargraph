259f0cf959186eb92de61862bdf69c3b2cfe28ee
PantelisElinas <pantelis.elinas@data61.csiro.au>
2019-02-04 14:25:50 +1100

    Black formatting.

--- stellargraph/__init__.py
+++ stellargraph/__init__.py
@@ -11,5 +11,5 @@ from .version import __version__
 # Top-level imports
 from stellargraph.core.graph import StellarGraph, StellarDiGraph
 from stellargraph.core.schema import GraphSchema
-from stellargraph.core.calibration import TemperatureCalibration, IsotonicCalibration, plot_reliability_diagram, \
-    expected_calibration_error
+from stellargraph.core.calibration import TemperatureCalibration, IsotonicCalibration
+from stellargraph.core.calibration import plot_reliability_diagram, expected_calibration_error
