c9a93e237bcac57f107e9a5131535b366eddc774
TheDen <deniskhoshaba@gmail.com>
2019-03-12 11:32:23 +1100

    add python 3.7.2 step to BK

--- .buildkite/script.sh
+++ .buildkite/script.sh
@@ -3,13 +3,15 @@
 set -xeo pipefail
 
 upload_tests() {
-  buildkite-agent artifact upload "${BUILDKITE_BUILD_NUMBER}.xml" s3://${AWS_LOGS_BUCKET}/pytest/${BUILDKITE_BRANCH}/${BUILDKITE_BUILD_NUMBER}
+  buildkite-agent artifact upload "${BUILDKITE_BUILD_NUMBER}.xml" s3://"${AWS_LOGS_BUCKET}"/pytest/"${BUILDKITE_BRANCH}"/"${BUILDKITE_BUILD_NUMBER}"
 }
 
 pip install -q --no-cache-dir -r requirements.txt -e .
-py.test -ra --cov=stellargraph tests/ --doctest-modules --doctest-modules --cov-report=term-missing -p no:cacheprovider --junitxml=./${BUILDKITE_BUILD_NUMBER}.xml
+py.test -ra --cov=stellargraph tests/ --doctest-modules --doctest-modules --cov-report=term-missing -p no:cacheprovider --junitxml=./"${BUILDKITE_BUILD_NUMBER}".xml
 coveralls
 
 if [ "${BUILDKITE_BRANCH}" = "develop"  ] || [ "${BUILDKITE_BRANCH}" = "master" ] ; then
-	upload_tests
+  if [ -n "${UPLOAD_PYTEST}" ] ; then
+    upload_tests
+  fi
 fi
