cf31fae320a5c7f8952e8b547aec0d2c35b49a92
TheDen <deniskhoshaba@gmail.com>
2019-04-17 13:00:12 +1000

    parallelise steps

--- .buildkite/steps/manage-images.sh
+++ .buildkite/steps/manage-images.sh
@@ -11,7 +11,7 @@ build_push() {
   docker push stellargraph/"${image}":"${tag}"
 }
 
-build_publish_images() {
+build_publish_numbered() {
   local tag="$1"
   shift
 
@@ -19,6 +19,41 @@ build_publish_images() {
     echo "+++ :docker: build and push $i"
     build_push "$i" "$tag"
   done
+}
+
+publish_images() {
+  local numbered_tag="$1"
+  local published_tag="$2"
+  shift 2
+
+  for i in "$@"; do
+    docker pull stellargraph/"$i":"$numbered_tag"
+    docker tag stellargraph/"$i":"$numbered_tag" stellargraph/"$i":"$published_tag"
+    docker push stellargraph/"$i":"$published_tag"
+  done
+}
+
+delete_numbered() {
+  local version=$1
+  shift
+
+  set +x # don't print out the docker hub credentials
+  TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKER_LOGIN_USER}'", "password": "'${DOCKER_LOGIN_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+  authHeader="Authorization: JWT ${TOKEN}"
+
+  for image_name in "$@"; do
+    if [[ $version == *-${BUILDKITE_BUILD_NUMBER} ]]; then
+      # This is a numbered tag, so we should try to delete it
+      url="https://hub.docker.com/v2/repositories/stellargraph/${image_name}/tags/${version}"
+
+      if curl --fail -L -X DELETE -H "$authHeader" "$url"; then
+        echo "successfully deleted 'stellargraph/${image_name}:${version}'"
+      else
+        echo "failed to delete 'stellargraph/${image_name}:${version}' (probably never uploaded)"
+      fi
+      fi
+      #done
+    done
 }
 
 images=(
@@ -37,9 +72,18 @@ case "${BUILDKITE_BRANCH}" in
     ;;
 esac
 
+published_image_tag="${version}"
+numbered_image_tag="${published_image_tag}-${BUILDKITE_BUILD_NUMBER}"
+
 case "${action}" in
-  build-publish)
-    build_publish_images "${version}" "${images[@]}"
+  build-publish-numbered)
+    build_publish_numbered "${numbered_image_tag}" "${images[@]}"
+    ;;
+  publish-images)
+    publish_images "${numbered_image_tag}" "${published_image_tag}" "${images[@]}"
+    ;;
+  delete-numbered)
+    delete_numbered "${numbered_image_tag}" "${images[@]}"
     ;;
   *)
     echo "[ERROR] unknown argument '${action}'" >&2
