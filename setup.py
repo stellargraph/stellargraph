e16c45e8a844ba34ad3fb17014cc3f0bef49dd70
Geoff Jarrad <geoff.jarrad@data61.csiro.au>
2019-07-09 15:22:15 +0930

    Restricted tensorflow<2.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ URL = "https://github.com/stellargraph/stellargraph"
 # Required packages
 REQUIRES = [
     "keras>=2.2.4",
-    "tensorflow>=1.12",
+    "tensorflow>=1.12,<2.0",
     "numpy>=1.14",
     "scipy>=1.1.0",
     "networkx>=2.2",
