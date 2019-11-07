9fa0734e218c1e413e20b8775c83e4949736f4b4
PantelisElinas <pantelis.elinas@data61.csiro.au>
2019-11-07 11:40:48 +1100

    Removed self.method since the adjacency normalization is fixed and not done in a layer, for now.

--- stellargraph/layer/cluster_gcn.py
+++ stellargraph/layer/cluster_gcn.py
@@ -19,29 +19,29 @@ from tensorflow.keras import activations, initializers, constraints, regularizer
 from tensorflow.keras.layers import Input, Layer, Lambda, Dropout, Reshape
 
 from ..mapper import ClusterNodeGenerator
-from .misc import SqueezedSparseConversion
 from .preprocessing_layer import GraphPreProcessingLayer
 
 
 class ClusterGraphConvolution(Layer):
 
     """
-    Graph Convolution (GCN) Keras layer.
-    The implementation is based on the keras-gcn github repo https://github.com/tkipf/keras-gcn.
+    Cluster Graph Convolution (GCN) Keras layer.
+    The implementation is based on the keras-gcn github repo https://github.com/tkipf/keras-gcn
+    GCN Keras layer.
 
-    Original paper: Semi-Supervised Classification with Graph Convolutional Networks. Thomas N. Kipf, Max Welling,
-    International Conference on Learning Representations (ICLR), 2017 https://github.com/tkipf/gcn
+    Original paper: Cluster-GCN: An Efficient Algorithm for Training Deep and Large Graph
+    Convolutional Networks, W. Chiang, X. Liu, S. Si, Y. Li, S. Bengio, and C. Hsieh,
+    KDD, 2019, https://arxiv.org/abs/1905.07953
 
     Notes:
       - The inputs are tensors with a batch dimension of 1:
-        Keras requires this batch dimension, and for full-batch methods
-        we only have a single "batch".
+        Keras requires this batch dimension.
 
       - There are three inputs required, the node features, the output
         indices (the nodes that are to be selected in the final layer)
-        and the normalized graph Laplacian matrix
+        and the normalized graph adjacency matrix.
 
-      - This class assumes that the normalized Laplacian matrix is passed as
+      - This class assumes that the normalized graph adjacency matrix is passed as
         input to the Keras methods.
 
       - The output indices are used when ``final_layer=True`` and the returned outputs
@@ -58,7 +58,6 @@ class ClusterGraphConvolution(Layer):
                             if True it returns the subset specified by the indices passed to it.
         kernel_initializer (str): name of layer bias f the initializer for kernel parameters (weights)
         bias_initializer (str): name of the initializer for bias
-        attn_kernel_initializer (str): name of the initializer for attention kernel
         kernel_regularizer (str): name of regularizer to be applied to layer kernel. Must be a Keras regularizer.
         bias_regularizer (str): name of regularizer to be applied to layer bias. Must be a Keras regularizer.
         activity_regularizer (str): not used in the current implementation
@@ -195,11 +194,6 @@ class ClusterGraphConvolution(Layer):
             Keras Tensor that represents the output of the layer.
         """
         features, out_indices, *As = inputs
-        #batch_dim, n_nodes, _ = K.int_shape(features)
-        # if batch_dim != 1:
-        #     raise ValueError(
-        #         "Currently full-batch methods only support a batch dimension of one"
-        #     )
 
         # Remove singleton batch dimension
         features = K.squeeze(features, 0)
@@ -219,10 +213,6 @@ class ClusterGraphConvolution(Layer):
         if self.final_layer:
             # Select the indices that are non-zero
             output = K.gather(output, out_indices)
-
-        # Add batch dimension back if we removed it
-        # print("BATCH DIM:", batch_dim)
-        # if batch_dim == 1:
         else:
             output = K.expand_dims(output, 0)
 
@@ -231,55 +221,45 @@ class ClusterGraphConvolution(Layer):
 
 class ClusterGCN:
     """
-    A stack of Graph Convolutional layers that implement a graph convolution network model
-    as in https://arxiv.org/abs/1609.02907
+    A stack of Cluster Graph Convolutional layers that implement a cluster graph convolution network
+    model as in https://arxiv.org/abs/1905.07953
 
     The model minimally requires specification of the layer sizes as a list of ints
     corresponding to the feature dimensions for each hidden layer,
     activation functions for each hidden layers, and a generator object.
 
     To use this class as a Keras model, the features and pre-processed adjacency matrix
-    should be supplied using the :class:`FullBatchNodeGenerator` class. To have the appropriate
-    pre-processing the generator object should be instantiated as follows::
+    should be supplied using the :class:`ClusterNodeGenerator` class.
 
-        generator = FullBatchNodeGenerator(G, method="gcn")
-
-    Note that currently the GCN class is compatible with both sparse and dense adjacency
-    matrices and the :class:`FullBatchNodeGenerator` will default to sparse.
-
-    For more details, please see the GCN demo notebook:
-    demos/node-classification/gat/gcn-cora-node-classification-example.ipynb
+    For more details, please see the Cluster-GCN demo notebook:
+    demos/node-classification/clustergcn/cluster-gcn-node-classification.ipynb
 
     Notes:
       - The inputs are tensors with a batch dimension of 1. These are provided by the \
-        :class:`FullBatchNodeGenerator` object.
+        :class:`ClusterNodeGenerator` object.
 
-      - This assumes that the normalized Lapalacian matrix is provided as input to
-        Keras methods. When using the :class:`FullBatchNodeGenerator` specify the
-        ``method='gcn'`` argument to do this pre-processing.
-
-      - The nodes provided to the :class:`FullBatchNodeGenerator.flow` method are
+      - The nodes provided to the :class:`ClusterNodeGenerator.flow` method are
         used by the final layer to select the predictions for those nodes in order.
         However, the intermediate layers before the final layer order the nodes
         in the same way as the adjacency matrix.
 
     Examples:
-        Creating a GCN node classification model from an existing :class:`StellarGraph`
+        Creating a Cluster-GCN node classification model from an existing :class:`StellarGraph`
         object ``G``::
 
-            generator = FullBatchNodeGenerator(G, method="gcn")
-            gcn = GCN(
-                    layer_sizes=[32, 4],
-                    activations=["elu","softmax"],
-                    generator=generator,
-                    dropout=0.5
+            generator = ClusterNodeGenerator(G, clusters=None, k=10, q=2)
+            cluster_gcn = ClusterGCN(
+                             layer_sizes=[32, 4],
+                             activations=["elu","softmax"],
+                             generator=generator,
+                             dropout=0.5
                 )
-            x_inp, predictions = gcn.node_model()
+            x_inp, predictions = cluster_gcn.build()
 
     Args:
         layer_sizes (list of int): list of output sizes of GCN layers in the stack
         activations (list of str): list of activations applied to each layer's output
-        generator (FullBatchNodeGenerator): an instance of FullBatchNodeGenerator class constructed on the graph of interest
+        generator (ClusterNodeGenerator): an instance of ClusterNodeGenerator class constructed on the graph of interest
         bias (bool): toggles an optional bias in GCN layers
         dropout (float): dropout rate applied to input features of each GCN layer
         kernel_regularizer (str): normalization applied to the kernels of GCN layers
@@ -306,17 +286,8 @@ class ClusterGCN:
         self.kernel_regularizer = kernel_regularizer
         self.generator = generator
         self.support = 1
-        self.method = "gcn"  # generator.method
 
-        # Check if the generator is producing a sparse matrix
-        # self.use_sparse = generator.use_sparse
-        if self.method == "none":
-            # This will no longer work since  the shape of the adjacency matrix is not
-            # fixed.
-            self.graph_norm_layer = GraphPreProcessingLayer(
-                num_of_nodes=self.generator.Aadj.shape[0]
-            )
-        # Initialize a stack of GCN layers
+        # Initialize a stack of Cluster GCN layers
         n_layers = len(self.layer_sizes)
         self._layers = []
         for ii in range(n_layers):
@@ -354,26 +325,15 @@ class ClusterGCN:
         """
         x_in, out_indices, *As = x
 
-        # Currently we require the batch dimension to be one for full-batch methods
-        # batch_dim, n_nodes, _ = K.int_shape(x_in)
-        # if batch_dim != 1:
-        #     raise ValueError(
-        #          "Currently full-batch methods only support a batch dimension of one"
-        #      )
-
         Ainput = [Lambda(lambda A: K.squeeze(A, 0))(A) for A in As]
-        #Ainput = [A for A in As]
 
-        # TODO: Support multiple matrices?
         if len(Ainput) != 1:
             raise NotImplementedError(
                 "The Cluster GCN method currently only accepts a single matrix for each minibatch"
             )
 
         h_layer = x_in
-        if self.method == "none":
-            # For GCN, if no preprocessing has been done, we apply the preprocessing layer to perform that.
-            Ainput = [self.graph_norm_layer(Ainput[0])]
+
         for layer in self._layers:
             if isinstance(layer, ClusterGraphConvolution):
                 # For a GCN layer add the matrix and output indices
@@ -387,18 +347,19 @@ class ClusterGCN:
 
     def build(self):
         """
-        Builds a GCN model for node prediction
+        Builds a Cluster-GCN model for node prediction
 
         Returns:
-            tuple: `(x_inp, x_out)`, where `x_inp` is a list of two Keras input tensors for the GCN model (containing node features and graph laplacian),
-            and `x_out` is a Keras tensor for the GCN model output.
+            tuple: `(x_inp, x_out)`, where `x_inp` is a list of two Keras input tensors for the
+            Cluster- GCN model (containing node features and normalized adjacency matrix),
+            and `x_out` is a Keras tensor for the Cluster-GCN model output.
         """
         # Placeholder for node features
         N_feat = self.generator.features.shape[1]
 
         # Inputs for features & target indices
         x_t = Input(batch_shape=(1, None, N_feat))
-        out_indices_t = Input(batch_shape=(1, None, None), dtype="int32")  # was (1, None, None)
+        out_indices_t = Input(batch_shape=(1, None, None), dtype="int32")
 
         # Placeholders for the dense adjacency matrix
         A_m = Input(batch_shape=(1, None, None))
