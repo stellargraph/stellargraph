172ea055af126543eff77a15825fc4893bf492ec
Geoff Jarrad <geoff.jarrad@data61.csiro.au>
2019-11-19 12:12:55 +1030

    Reblacked code.

--- tests/mapper/test_node_mappers.py
+++ tests/mapper/test_node_mappers.py
@@ -366,7 +366,9 @@ def test_nodemapper_isolated_nodes():
     G = example_graph_3(feature_size=n_feat, n_nodes=6, n_isolates=1, n_edges=20)
 
     # Check connectedness
-    assert isinstance(G, NetworkXStellarGraph)  # XXX Hack - Only works for NetworkXStellarGraph instances
+    assert isinstance(
+        G, NetworkXStellarGraph
+    )  # XXX Hack - Only works for NetworkXStellarGraph instances
     Gnx = G._graph
     ccs = list(nx.connected_components(Gnx))
     assert len(ccs) == 2
