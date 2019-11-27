4c8decf489e876023ecc554459e163110c17a5ec
Huon Wilson <Huon.Wilson@data61.csiro.au>
2019-11-27 17:46:34 +1100

    Fix copyright headers

--- .buildkite/steps/check-copyright-headers.sh
+++ .buildkite/steps/check-copyright-headers.sh
@@ -8,7 +8,14 @@ temp="$(mktemp)"
 copyrightRegex="# Copyright [0-9-]* Data61, CSIRO"
 
 echo "--- checking files have copyright headers"
-find . -name "*.py" -exec grep -L "$copyrightRegex" {} + | tee "$temp"
+# Some files shouldn't have our copyright header and so are ignored
+find . \( \
+  -name "*.py" \
+  -a ! -path "./demos/link-prediction/random-walks/utils/node2vec/node2vec.py" \
+  -a ! -path "./demos/link-prediction/random-walks/utils/node2vec/main.py" \
+  -a ! -path "./docs/conf.py" \
+  \) \
+  -exec grep -L "$copyrightRegex" {} + | tee "$temp"
 
 if [ -s "$temp" ]; then
   echo "^^^ +++"
