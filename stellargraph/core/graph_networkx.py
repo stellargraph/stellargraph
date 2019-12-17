f8312323c4dd34a5e27ac6892a08d76270f4ee6f
Daokun Zhang <daokunzhang2015@gmail.com>
2019-12-17 19:22:19 +1100

    make the get_index_for_nodes fun support networks without node features

--- stellargraph/core/graph_networkx.py
+++ stellargraph/core/graph_networkx.py
@@ -311,6 +311,22 @@ class NetworkXStellarGraph(StellarGraph):
 
         else:
             data_index_maps = {}
+            nodes_by_type = {
+                nt: [
+                    n
+                    for n, ndata in self._graph.nodes(data=True)
+                    if ndata.get(self._node_type_attr, self._node_type_default) == nt
+                ]
+                for nt in node_types
+            }
+            for nt in node_types:
+                nt_node_list = nodes_by_type[nt]
+
+                # Add None to node list as ID of unknown nodes
+                nt_node_list.append(None)
+
+                # Create map between node id and index (including None)
+                data_index_maps[nt] = {nid: ii for ii, nid in enumerate(nt_node_list)}
             data_arrays = {}
 
         # TODO: What other convenience attributes do we need?
