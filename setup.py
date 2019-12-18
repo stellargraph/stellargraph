e8648282ca8a7bff063046a118aa6456eb7de7a7
Yuriy Tyshetskiy <yuriy.tyshetskiy@data61.csiro.au>
2019-12-18 16:12:25 +1100

    added tensorflow-gpu to extra requirements

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ URL = "https://github.com/stellargraph/stellargraph"
 
 # Required packages
 REQUIRES = [
+    "tensorflow>=1.14,<1.15",
     "numpy>=1.14",
     "scipy>=1.1.0",
     "networkx>=2.2",
@@ -42,7 +43,7 @@ REQUIRES = [
 EXTRAS_REQURES = {
     "demos": ["numba", "jupyter", "seaborn"],
     "test": ["pytest", "pytest-benchmark"],
-    "cpu": ["tensorflow>=1.14,<1.15"],
+    "gpu": ["tensorflow-gpu>=1.14,<1.15"],
 }
 
 # Long description
