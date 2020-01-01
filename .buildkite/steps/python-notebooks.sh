1d2ddce94be47ca624a4adc6dee4921a99c2bb7d
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-01-02 10:09:51 +1100

    Remove the parallelism

--- .buildkite/steps/python-notebooks.sh
+++ .buildkite/steps/python-notebooks.sh
@@ -14,11 +14,6 @@ done < <(find "$PWD" -name "*.ipynb" -print0)
 
 NUM_NOTEBOOKS_TOTAL=${#NOTEBOOKS[@]}
 
-if [ "$NUM_NOTEBOOKS_TOTAL" -ne "$SPLIT" ]; then
-  echo "Buildkite step parallelism is set to $SPLIT but found $NUM_NOTEBOOKS_TOTAL notebooks. Please update parallelism to match the number of notebooks!"
-  exit 1
-fi
-
 echo "--- :python: installing notebooks dependencies"
 pip install -q --no-cache-dir -r requirements.txt -e .
 
@@ -27,14 +22,15 @@ pip freeze
 
 echo "--- :python: preparing to run notebooks"
 
-f=${NOTEBOOKS[$INDEX]}
-
-echo "+++ :python: running $f"
-cd "$(dirname "$f")"
-# run the notebook, saving it back to where it was, printing everything
 exitCode=0
-papermill --log-output "$f" "$f" || exitCode=$?
+for f in "${NOTEBOOKS[@]}"; do
+  echo "+++ :python: running $f"
+  cd "$(dirname "$f")"
+  # run the notebook, saving it back to where it was, printing everything
+  papermill --log-output "$f" "$f" || exitCode=$?
 
-# and also upload the notebook with outputs, for someone to download and look at
-buildkite-agent artifact upload "$(basename "$f")"
+  # and also upload the notebook with outputs, for someone to download and look at
+  buildkite-agent artifact upload "$(basename "$f")"
+done
+# any failed?
 exit $exitCode
