0c1e1268d2a40edda278a521f4c5a506c48b7c7f
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-01-02 10:37:55 +1100

    Use junit-annotate buildkite plugin to highlight failures on CI

--- .buildkite/steps/script.sh
+++ .buildkite/steps/script.sh
@@ -2,14 +2,19 @@
 
 set -xeo pipefail
 
+junit_file="junit/${BUILDKITE_JOB_ID}.xml"
+
 upload_tests() {
-  buildkite-agent artifact upload "${BUILDKITE_BUILD_NUMBER}.xml" "s3://${AWS_LOGS_BUCKET}/pytest/${BUILDKITE_BRANCH}/${BUILDKITE_BUILD_NUMBER}"
+  buildkite-agent artifact upload "$junit_file" "s3://${AWS_LOGS_BUCKET}/pytest/${BUILDKITE_BRANCH}/${BUILDKITE_BUILD_NUMBER}"
 }
 
 pip install -q --no-cache-dir -r requirements.txt -e .
-py.test -ra --cov=stellargraph tests/ --doctest-modules --doctest-modules --cov-report=term-missing -p no:cacheprovider --junitxml=./"${BUILDKITE_BUILD_NUMBER}".xml
+py.test -ra --cov=stellargraph tests/ --doctest-modules --doctest-modules --cov-report=term-missing -p no:cacheprovider --junitxml="./${junit_file}"
 coveralls
 
 if [ "${BUILDKITE_BRANCH}" = "develop" ] || [ "${BUILDKITE_BRANCH}" = "master" ]; then
   upload_tests
 fi
+
+# upload the JUnit file for the junit annotation plugin
+buildkite-agent artifact upload "${junit_file}"
