9ed7c2df966d9e1190b3eec1862c33286a37d650
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-01-02 10:56:46 +1100

    Add sections to make buildkite output clearer (#578)
    This uses https://buildkite.com/docs/pipelines/managing-log-output to group
    commands and output on buildkite, and also show the time for each one.

--- .buildkite/steps/script.sh
+++ .buildkite/steps/script.sh
@@ -6,10 +6,16 @@ upload_tests() {
   buildkite-agent artifact upload "${BUILDKITE_BUILD_NUMBER}.xml" "s3://${AWS_LOGS_BUCKET}/pytest/${BUILDKITE_BRANCH}/${BUILDKITE_BUILD_NUMBER}"
 }
 
+echo "--- installing dependencies"
 pip install -q --no-cache-dir -r requirements.txt -e .
+
+echo "+++ running tests"
 py.test -ra --cov=stellargraph tests/ --doctest-modules --doctest-modules --cov-report=term-missing -p no:cacheprovider --junitxml=./"${BUILDKITE_BUILD_NUMBER}".xml
+
+echo "--- uploading coveralls"
 coveralls
 
 if [ "${BUILDKITE_BRANCH}" = "develop" ] || [ "${BUILDKITE_BRANCH}" = "master" ]; then
+  echo "--- uploading JUnit"
   upload_tests
 fi
