212de77111db116db60802d5b2de787be9a3f2b3
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-01-10 11:18:28 +1100

    Wrap annotation filenames in backticks

--- .buildkite/steps/check-copyright-headers.sh
+++ .buildkite/steps/check-copyright-headers.sh
@@ -17,7 +17,7 @@ annotate_error() {
   buildkite-agent annotate --context "$context" --style error << EOF
 ${msg}:
 
-$(sed s/^/- / "$temp")
+$(sed 's/^\(.*\)$/- `\1`/' "$temp")
 EOF
 
   exitCode=1
