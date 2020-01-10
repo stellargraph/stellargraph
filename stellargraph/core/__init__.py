b4ec1acefbbc3f930048a7b4700c3b557069e056
pit070 <timpitman@users.noreply.github.com>
2020-01-10 15:44:42 +1100

    updated copyright to 2020

--- stellargraph/core/__init__.py
+++ stellargraph/core/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2018 Data61, CSIRO
+# Copyright 2018-2020 Data61, CSIRO
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
