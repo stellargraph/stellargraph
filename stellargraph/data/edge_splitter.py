7d1040d15516aa69082027ee4f053a6f8dde220c
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-01-15 10:53:39 +1100

    Remove if with vacuous condition from edge_splitter

--- stellargraph/data/edge_splitter.py
+++ stellargraph/data/edge_splitter.py
@@ -388,16 +388,13 @@ class EdgeSplitter(object):
         else:
             all_edges = list(self.g.edges())
 
-        if edge_attribute_label is None or edge_attribute_threshold is None:
+        if edge_attribute_label is None and edge_attribute_threshold is None:
             # filter by edge_label
             edges_with_label = [
                 e for e in all_edges if self.g.get_edge_data(*e)["label"] == edge_label
             ]
 
-        elif (
-            edge_attribute_threshold is not None
-            and edge_attribute_threshold is not None
-        ):
+        else:
             # filter by edge label, edge attribute and threshold value
             edge_attribute_threshold_dt = datetime.datetime.strptime(
                 edge_attribute_threshold, "%d/%m/%Y"
@@ -413,8 +410,6 @@ class EdgeSplitter(object):
                     > edge_attribute_threshold_dt
                 )
             ]
-        else:
-            raise ValueError("Invalid parameters")  # not the most informative error!
 
         return edges_with_label
 
