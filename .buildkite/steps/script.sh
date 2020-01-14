831f6dbfe801e2b378472cc1d50136b3e698c312
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-01-15 09:45:07 +1100

    Non-user install

--- .buildkite/steps/script.sh
+++ .buildkite/steps/script.sh
@@ -9,7 +9,7 @@ upload_tests() {
 }
 
 echo "--- installing dependencies"
-pip install -q --no-cache-dir -r requirements.txt . --user
+pip install -q --no-cache-dir -r requirements.txt .
 
 echo "--- listing dependency versions"
 pip freeze
