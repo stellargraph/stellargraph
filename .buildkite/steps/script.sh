c9370d8279df70edb652db9955e35e784a2aa88b
pit070 <pitman.tim@gmail.com>
2020-01-30 16:50:07 +1100

    remove annotation

--- .buildkite/steps/script.sh
+++ .buildkite/steps/script.sh
@@ -18,19 +18,6 @@ echo "+++ running tests"
 exitCode=$?
 py.test -ra --cov=stellargraph tests/ --doctest-modules --cov-report=xml -p no:cacheprovider --junitxml="./${junit_file}" || exitCode=$?
 
-echo "--- annotating coverage"
-FILE="coverage.xml"
-
-show_coverage() {
-  echo "--- :arrow_up::buildkite: annotating build"
-  buildkite-agent annotate --style "info" --context "coverage" << EOF
-Code coverage:
-- branch: $(xmllint --xpath "100 * string(//coverage/@line-rate)" "$FILE")%
-EOF
-}
-
-show_coverage
-
 echo "--- :coverage::codecov::arrow_up: uploading coverage to codecov.io"
 bash <(curl https://codecov.io/bash)
 
