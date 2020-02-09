52d912a73ec97d0335656ad0a9514fd3db2a7c05
pit070 <pitman.tim@gmail.com>
2020-02-10 10:58:13 +1100

    formatting and add soft fail

--- .buildkite/steps/test-demo-notebooks.sh
+++ .buildkite/steps/test-demo-notebooks.sh
@@ -24,22 +24,21 @@ if [ "$NUM_NOTEBOOKS_TOTAL" -ne "$SPLIT" ]; then
   exit 1
 fi
 
-
 echo "--- installing dependencies"
 pip install -q --no-cache-dir '.[demos]'
 
 echo "--- listing dependency versions"
 pip freeze
 
-
 f=${NOTEBOOKS[$INDEX]}
 
 echo "+++ :python: running $f"
 case $(basename "$f") in
   'attacks_clustering_analysis.ipynb' | 'ensemble-node-classification-example.ipynb' | 'hateful-twitters-interpretability.ipynb' | 'hateful-twitters.ipynb' | 'stellargraph-attri2vec-DBLP.ipynb' | \
-  'stellargraph-metapath2vec.ipynb' | 'node-link-importance-demo-gat.ipynb' | 'node-link-importance-demo-gcn-sparse.ipynb' | 'rgcn-aifb-node-classification-example.ipynb')
+    'stellargraph-metapath2vec.ipynb' | 'node-link-importance-demo-gat.ipynb' | 'node-link-importance-demo-gcn-sparse.ipynb' | 'rgcn-aifb-node-classification-example.ipynb')
     # FIXME: these notebooks do not yet work on CI (#818 - datasets can't be downloaded)
     # FIXME: these notebooks do not yet work on CI (#819 - out of memory)
+    exit 2
     ;;
   *) # fine, run this one
     cd "$(dirname "$f")"
