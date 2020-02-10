ab1f075e2322946396268dce7d1e40e11c99f241
pit070 <pitman.tim@gmail.com>
2020-02-10 16:24:06 +1100

    disable node-link-importance-demo-gcn.ipynb due to oom

--- .buildkite/steps/test-demo-notebooks.sh
+++ .buildkite/steps/test-demo-notebooks.sh
@@ -35,7 +35,7 @@ f=${NOTEBOOKS[$INDEX]}
 echo "+++ :python: running $f"
 case $(basename "$f") in
   'attacks_clustering_analysis.ipynb' | 'hateful-twitters-interpretability.ipynb' | 'hateful-twitters.ipynb' | 'stellargraph-attri2vec-DBLP.ipynb' | \
-    'node-link-importance-demo-gat.ipynb' | 'node-link-importance-demo-gcn-sparse.ipynb' | 'rgcn-aifb-node-classification-example.ipynb' | \
+    'node-link-importance-demo-gat.ipynb' | 'node-link-importance-demo-gcn.ipynb' | 'node-link-importance-demo-gcn-sparse.ipynb' | 'rgcn-aifb-node-classification-example.ipynb' | \
     'stellargraph-metapath2vec.ipynb' | \
     'ensemble-node-classification-example.ipynb' | 'cora-gcn-links-example.ipynb')
     # FIXME: these notebooks do not yet work on CI (#818 - datasets can't be downloaded)
