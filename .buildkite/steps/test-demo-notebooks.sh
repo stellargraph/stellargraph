b86dcecb068a88b084e90acc89795ed0a6d71267
pit070 <pitman.tim@gmail.com>
2020-02-12 14:55:39 +1100

    reduce estimators for ensembles

--- .buildkite/steps/test-demo-notebooks.sh
+++ .buildkite/steps/test-demo-notebooks.sh
@@ -51,7 +51,7 @@ echo "+++ :python: running $f"
 cd "$(dirname "$f")"
 # run the notebook, saving it back to where it was, printing everything
 exitCode=0
-papermill --execution-timeout=600 -p epochs 2 -p walk_length 5 -p batch_size 5 --log-output "$f" "$f" || exitCode=$?
+papermill --execution-timeout=600 -p epochs 2 -p walk_length 5 -p batch_size 5 -p n_estimators 2 --log-output "$f" "$f" || exitCode=$?
 
 # and also upload the notebook with outputs, for someone to download and look at
 buildkite-agent artifact upload "$(basename "$f")"
