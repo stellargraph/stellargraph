df35baa7ecf3bee6bf6ea20dd4ca88357cf8cfc2
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-02-12 14:15:23 +1100

    WIP2

--- stellargraph/data/edge_splitter2.py
+++ stellargraph/data/edge_splitter2.py
@@ -74,22 +74,25 @@ class EdgeSplitter:
                 p, method, edge_label, edge_attribute_threshold, random
             )
 
-        return self._train_test_split_homogeneous(
-            p, method, probs, edge_attribute_threshold, random
-        )
+        return self._train_test_split_homogeneous(p, method, probs, random)
 
     def _train_test_split_heterogeneous(
         self, p, method, edge_label, edge_attribute_threshold, random
     ):
         raise NotImplementedError()
 
-    def _negative_global(self, num_test_edges, random):
+    def _nonnegative_edges(self):
         edges = self.reference_graph.edges()
 
         nonnegative_edges = set(edges)
         nonnegative_edges.update((t, s) for s, t in edges)
         # self-loops don't count as negative edges either
         nonnegative_edges.update((n, n) for n in self.reference_graph.nodes())
+
+        return nonnegative_edges
+
+    def _negative_global(self, num_test_edges, random):
+        nonnegative_edges = self._nonnegative_edges()
 
         negative_test_edges = set()
         source = np.asarray(self.reference_graph.nodes())
@@ -99,10 +102,11 @@ class EdgeSplitter:
         for _ in range(num_iter):
             random.shuffle(source)
             random.shuffle(target)
-            for u, v in zip(source, target):
-                if (u, v) not in nonnegative_edges:
-                    negative_test_edges.add((u, v))
+            for edge in zip(source, target):
+                if edge in nonnegative_edges:
+                    continue
 
+                negative_test_edges.add(edge)
                 if len(negative_test_edges) == num_test_edges:
                     return list(negative_test_edges)
 
@@ -110,9 +114,48 @@ class EdgeSplitter:
             f"failed to sample {num_test_edges} negative edges. Consider using a smaller value for p."
         )
 
-    def _train_test_split_homogeneous(
-        self, p, method, probs, edge_attribute_threshold, random
-    ):
+    def _negative_local(self, num_test_edges, probs, random):
+        nonnegative_edges = self._nonnegative_edges()
+        negative_test_edges = set()
+
+        source = np.asarray(self.reference_graph.nodes())
+
+        num_iter = int(np.ceil(num_test_edges) / len(source)) + 1
+        for _ in range(num_iter):
+            random.shuffle(source)
+
+            target_node_distances = random.choice(len(probs), len(source), p=probs)
+            for start_node, target_depth in zip(source, target_node_distances):
+                # depth first search
+                visited = set()
+                nodes_stack = [(start_node, 0)]
+                while nodes_stack:
+                    curr_node, depth = nodes_stack.pop()
+                    if curr_node in visited:
+                        continue
+
+                    visited.add(next_node)
+                    if depth == target_depth:
+                        edge = (start_node, curr_node)
+                        if edge in nonnegative_edges:
+                            continue
+
+                        negative_test_edges.add(edge)
+                        if len(negative_test_edges) == num_test_edges:
+                            return list(negative_test_edges)
+
+                        # found an edge for this start node
+                        break
+                    else:
+                        neighbours = list(self.g.neighbors(curr_node))
+                        random.shuffle(neighbours)
+                        nodes_stack.extend((n, depth + 1) for n in neighbours)
+
+        raise ValueError(
+            f"failed to sample {num_test_edges} negative edges. Consider using a smaller value for p."
+        )
+
+    def _train_test_split_homogeneous(self, p, method, probs, random):
         num_edges = self.g.number_of_edges()
 
         num_test_edges = int(num_edges * p)
@@ -120,25 +163,28 @@ class EdgeSplitter:
         random_edge_ilocs = random.permutation(num_edges)
 
         positive_test_ilocs = random_edge_ilocs[:num_test_edges]
-        remaining_ilocs = random_edge_ilocs[num_test_edges:]
+        train_ilocs = random_edge_ilocs[num_test_edges:]
 
         if method == "global":
             negative_test_edges = _negative_global(num_test_edges, random)
         else:
-            raise NotImplementedError()
+            negative_test_edges = _negative_local(num_test_edges, probs, random)
 
         node_type = next(iter(self.g.node_types))
         raw_edges = self.reference_graph._edges
-        remaining_edge_data = EdgeData(
+        train_edge_data = EdgeData(
             {
                 node_type: pd.DataFrame(
                     {
-                        SOURCE: raw_edges.sources[remaining_ilocs],
-                        TARGET: raw_edges.targets[remaining_ilocs],
-                        WEIGHT: raw_edges.weights[remaining_ilocs],
+                        SOURCE: raw_edges.sources[train_ilocs],
+                        TARGET: raw_edges.targets[train_ilocs],
+                        WEIGHT: raw_edges.weights[train_ilocs],
                     }
                 )
             }
+        )
+        train_graph = type(self.g)(
+            _internal_raw_data=(self.reference_graph._nodes, train_edge_data)
         )
 
         positive_test_edges = list(
@@ -151,10 +197,4 @@ class EdgeSplitter:
         assert len(positive_test_edges) == len(negative_test_edges) == num_test_edges
         test_labels = np.array([1, 0]).repeat(num_test_edges)
 
-        return (
-            type(self.g)(
-                _internal_raw_data=(self.reference_graph._nodes, remaining_edge_data)
-            ),
-            test_edges,
-            test_labels,
-        )
+        return train_graph, test_edges, test_labels
