cadf21f6a6a945d4e1adcaf9c6506842bb463fc6
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-02-28 14:23:18 +1100

    Add AIFB.load to automatically parse and load the data

--- .buildkite/steps/test-demo-notebooks.sh
+++ .buildkite/steps/test-demo-notebooks.sh
@@ -33,7 +33,7 @@ f=${NOTEBOOKS[$INDEX]}
 
 case $(basename "$f") in
   'attacks_clustering_analysis.ipynb' | 'hateful-twitters-interpretability.ipynb' | 'hateful-twitters.ipynb' | 'stellargraph-attri2vec-DBLP.ipynb' | \
-    'node-link-importance-demo-gat.ipynb' | 'node-link-importance-demo-gcn.ipynb' | 'node-link-importance-demo-gcn-sparse.ipynb' | 'rgcn-aifb-node-classification-example.ipynb' | \
+    'node-link-importance-demo-gat.ipynb' | 'node-link-importance-demo-gcn.ipynb' | 'node-link-importance-demo-gcn-sparse.ipynb' | \
     'directed-graphsage-on-cora-neo4j-example.ipynb' | 'undirected-graphsage-on-cora-neo4j-example.ipynb' | 'load-cora-into-neo4j.ipynb' | \
     'stellargraph-metapath2vec.ipynb')
     # These notebooks do not yet work on CI:
