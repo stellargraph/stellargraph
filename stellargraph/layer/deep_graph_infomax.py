5fc94916528b8e4b8fc4d9dba06853c03280fc1a
kieranricardo <kieranricardo@hotmail.com>
2020-03-04 15:27:25 +1100

    refactored infomax code as a function that accepts a GCN model as a param

--- stellargraph/layer/deep_graph_infomax.py
+++ stellargraph/layer/deep_graph_infomax.py
@@ -22,9 +22,11 @@ from ..core.experimental import experimental
 
 
 __all__ = [
-    "GCNInfoMax",
+    "fullbatch_infomax_node_model",
+    "fullbatch_infomax_embedding_model",
 ]
 
+_NODE_FEATS = "GCN_INFO_MAX_NODE_FEATURES"
 
 class Discriminator(Layer):
     """
@@ -61,125 +63,72 @@ class Discriminator(Layer):
 
 
 @experimental(reason="lack of unit tests", issues=[1003])
-class GCNInfoMax(GCN):
+def fullbatch_infomax_node_model(base_model):
     """
-    A stack of Graph Convolutional layers that implement a Deep Graph Infomax model
-    as in https://arxiv.org/pdf/1809.10341.pdf
-
-    The model minimally requires specification of the layer sizes as a list of ints
-    corresponding to the feature dimensions for each hidden layer,
-    activation functions for each hidden layers, and a CorruptedFullBatchNodeGenerator object.
-
-    To use this class as a Keras model, the features, shuffled features, and a pre-processed adjacency matrix
-    should be supplied using the :class:`CorruptedFullBatchNodeGenerator` class for unsupervised training. After
-    training, a model that returns the node embeddings can be obtained using the `GCNInfoMax.embedding_model` function.
+    A function to create the the inputs and outputs for a Deep Graph Infomax model for unsupervised training.
 
     Args:
-        layer_sizes (list of int): Output sizes of GCN layers in the stack.
-        generator (FullBatchNodeGenerator): The generator instance.
-        bias (bool): If True, a bias vector is learnt for each layer in the GCN model.
-        dropout (float): Dropout rate applied to input features of each GCN layer.
-        activations (list of str or func): Activations applied to each layer's output;
-            defaults to ['relu', ..., 'relu'].
-        kernel_initializer (str or func, optional): The initialiser to use for the weights of each layer.
-        kernel_regularizer (str or func, optional): The regulariser to use for the weights of each layer.
-        kernel_constraint (str or func, optional): The constraint to use for the weights of each layer.
-        bias_initializer (str or func, optional): The initialiser to use for the bias of each layer.
-        bias_regularizer (str or func, optional): The regulariser to use for the bias of each layer.
-        bias_constraint (str or func, optional): The constraint to use for the bias of each layer.
+        base_model: the base stellargraph model class
+
+    Returns:
+        input and output layers for use with a keras model
     """
-    _NODE_FEATS = "GCN_INFO_MAX_NODE_FEATURES"
+    # Inputs for features
+    x_t = Input(batch_shape=(1, base_model.n_nodes, base_model.n_features))
+    # Inputs for shuffled features
+    x_corr = Input(batch_shape=(1, base_model.n_nodes, base_model.n_features))
 
-    def __init__(
-        self,
-        layer_sizes,
-        generator,
-        bias=True,
-        dropout=0.0,
-        activations=None,
-        kernel_initializer=None,
-        kernel_regularizer=None,
-        kernel_constraint=None,
-        bias_initializer=None,
-        bias_regularizer=None,
-        bias_constraint=None,
-    ):
+    out_indices_t = Input(batch_shape=(1, None), dtype="int32")
 
-        super().__init__(
-            layer_sizes,
-            generator,
-            bias=bias,
-            dropout=dropout,
-            activations=activations,
-            kernel_initializer=kernel_initializer,
-            kernel_regularizer=kernel_regularizer,
-            kernel_constraint=kernel_constraint,
-            bias_initializer=bias_initializer,
-            bias_regularizer=bias_regularizer,
-            bias_constraint=bias_constraint,
-        )
+    # Create inputs for sparse or dense matrices
+    if base_model.use_sparse:
+        # Placeholders for the sparse adjacency matrix
+        A_indices_t = Input(batch_shape=(1, None, 2), dtype="int64")
+        A_values_t = Input(batch_shape=(1, None))
+        A_placeholders = [A_indices_t, A_values_t]
 
-    def unsupervised_node_model(self):
-        """
-        A function to create the the inputs and outputs for a Deep Graph Infomax model for unsupervised training.
+    else:
+        # Placeholders for the dense adjacency matrix
+        A_m = Input(batch_shape=(1, base_model.n_nodes, base_model.n_nodes))
+        A_placeholders = [A_m]
 
-        Returns:
-            input and output layers for use with a keras model
-        """
-        # Inputs for features
-        x_t = Input(batch_shape=(1, self.n_nodes, self.n_features))
-        # Inputs for shuffled features
-        x_corr = Input(batch_shape=(1, self.n_nodes, self.n_features))
+    x_inp = [x_corr, x_t, out_indices_t] + A_placeholders
 
-        out_indices_t = Input(batch_shape=(1, None), dtype="int32")
+    node_feats = base_model([x_t, out_indices_t] + A_placeholders)
+    node_feats = Lambda(lambda x: K.squeeze(x, axis=0), name=_NODE_FEATS,)(
+        node_feats
+    )
 
-        # Create inputs for sparse or dense matrices
-        if self.use_sparse:
-            # Placeholders for the sparse adjacency matrix
-            A_indices_t = Input(batch_shape=(1, None, 2), dtype="int64")
-            A_values_t = Input(batch_shape=(1, None))
-            A_placeholders = [A_indices_t, A_values_t]
+    node_feats_corrupted = base_model([x_corr, out_indices_t] + A_placeholders)
+    node_feats_corrupted = Lambda(lambda x: K.squeeze(x, axis=0))(
+        node_feats_corrupted
+    )
 
-        else:
-            # Placeholders for the dense adjacency matrix
-            A_m = Input(batch_shape=(1, self.n_nodes, self.n_nodes))
-            A_placeholders = [A_m]
+    summary = Lambda(lambda x: tf.math.sigmoid(tf.math.reduce_mean(x, axis=0)))(
+        node_feats
+    )
 
-        x_inp = [x_corr, x_t, out_indices_t] + A_placeholders
+    discriminator = Discriminator()
+    scores = discriminator([node_feats, summary])
+    scores_corrupted = discriminator([node_feats_corrupted, summary])
 
-        node_feats = self([x_t, out_indices_t] + A_placeholders)
-        node_feats = Lambda(lambda x: K.squeeze(x, axis=0), name=self._NODE_FEATS,)(
-            node_feats
-        )
+    x_out = tf.stack([scores, scores_corrupted], axis=1)
 
-        node_feats_corrupted = self([x_corr, out_indices_t] + A_placeholders)
-        node_feats_corrupted = Lambda(lambda x: K.squeeze(x, axis=0))(
-            node_feats_corrupted
-        )
+    x_out = K.expand_dims(x_out, axis=0)
+    return x_inp, x_out
 
-        summary = Lambda(lambda x: tf.math.sigmoid(tf.math.reduce_mean(x, axis=0)))(
-            node_feats
-        )
 
-        discriminator = Discriminator()
-        scores = discriminator([node_feats, summary])
-        scores_corrupted = discriminator([node_feats_corrupted, summary])
+@experimental(reason="lack of unit tests", issues=[1003])
+def fullbatch_infomax_embedding_model(info_max_model):
+    """
+    A function to create the the inputs and outputs for an embedding model.
 
-        x_out = tf.stack([scores, scores_corrupted], axis=1)
+    Args:
+        info_max_model (keras.Model): the base Deep Graph Infomax model
+    Returns:
+        input and output layers for use with a keras model
+    """
+    x_emb_in = info_max_model.inputs
+    x_emb_out = info_max_model.get_layer(_NODE_FEATS).output
 
-        x_out = K.expand_dims(x_out, axis=0)
-        return x_inp, x_out
-
-    def embedding_model(self, model):
-        """
-        A function to create the the inputs and outputs for an embedding model.
-
-        Args:
-            model (keras.Model): the base Deep Graph Infomax model
-        Returns:
-            input and output layers for use with a keras model
-        """
-        x_emb_in = model.inputs
-        x_emb_out = model.get_layer(self._NODE_FEATS).output
-
-        return x_emb_in, x_emb_out
+    return x_emb_in, x_emb_out
