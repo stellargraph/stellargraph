9a067e5d5e727c57d949c8f8ee60979d8f872efa
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-03-04 16:16:44 +1100

    Fix left overs

--- tests/test_ensemble.py
+++ tests/test_ensemble.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import pandas as pd
 import pytest
 import numpy as np
 import tensorflow as tf
