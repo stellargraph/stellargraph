2afdfb1becbe3f3bee0bb7f9dd77bacb45a5f9e5
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-03-09 20:20:02 +1100

    Run neo4j on CI

---
+++ .buildkite/steps/test-neo4j-notebooks.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -xeo pipefail
+
+echo "--- :python: installing stellargraph"
+# install stellargraph itself and the dependencies necessary for neo4j, which (hopefully) won't
+# install any other ones. (The neo4j deps aren't installed into the docker image, so the main tests
+# run without them, validating they're properly optional.)
+pip install .[neo4j]
+
+echo "--- listing dependency versions"
+pip freeze
+
+directory="$PWD/demos/connector/neo4j"
+notebooks=('load-cora-into-neo4j.ipynb' 'directed-graphsage-on-cora-neo4j-example.ipynb' 'undirected-graphsage-on-cora-neo4j-example.ipynb')
+
+for name in "${notebooks[@]}"; do
+  .buildkite/steps/test-single-notebook.sh "$directory/$name"
+done
