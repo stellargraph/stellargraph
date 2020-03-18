be825b42d4583147a813b6a0c5d89929df71271f
kieranricardo <kieranricardo@hotmail.com>
2020-03-19 09:38:09 +1100

    fixed graph log likelihood signs + added docstrings + minor tweaks

--- stellargraph/losses.py
+++ stellargraph/losses.py
@@ -19,21 +19,30 @@ import tensorflow as tf
 from .core.experimental import experimental
 
 
-@experimental(reason="lack of unit tests", issues=[804])
-def graph_log_likelihood(y_true, y_pred):
+def graph_log_likelihood(batch_adj, wys_output):
     """
-    """
-    batch_adj = tf.gather(y_true, [0], axis=1)
+    Computes the graph log likelihood loss function as in https://arxiv.org/abs/1710.09599
 
-    expected_walks = tf.gather(y_pred, [0], axis=1)
-    scores = tf.gather(y_pred, [1], axis=1)
+    Args:
+        batch_adj: tensor with shape (batch_rows, 1, num_nodes) containing rows of the adjacency matrix
+        wys_output: tensor with shape (batch_rows, 2, num_nodes) containing the embedding outer product
+            scores (with shape (batch_rows, 1, num_nodes) and attentive expected random walk
+            (with shape (batch_rows, 1, num_nodes) concatenated.
+    Returns:
+        the graph log likelihood loss for the batch
+    """
+
+    batch_adj = tf.squeeze(batch_adj, axis=1)
+
+    expected_walks = tf.gather(wys_output, [0], axis=1)
+    scores = tf.gather(wys_output, [1], axis=1)
 
     adj_mask = tf.cast((batch_adj == 0), "float32")
 
     loss = tf.math.reduce_sum(
         tf.abs(
-            -(expected_walks * tf.math.log_sigmoid(scores))
-            - adj_mask * (-scores + tf.math.log_sigmoid(scores))
+            expected_walks * tf.math.log_sigmoid(scores)
+            - adj_mask * (tf.math.log_sigmoid(scores) - scores)
         )
     )
 
