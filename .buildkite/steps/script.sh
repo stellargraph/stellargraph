35acbc41c7c2547541b2531823b113e2b810011c
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-03-18 20:25:19 +1100

    Use --benchmark-disable

--- .buildkite/steps/script.sh
+++ .buildkite/steps/script.sh
@@ -15,12 +15,10 @@ exitCode=0
 
 echo "+++ running tests"
 # benchmarks on shared infrastructure like the CI machines are usually unreliable (high variance),
-# so there's no point spending too much time. It's good to check that the benchmark does run
-# successfully as a benchmark (that is, multiple iterations, not just once, as a test with
-# --benchmark-disable)
-small_benchmarks=("--benchmark-max-time=0.01" "--benchmark-warmup=off" "--benchmark-min-rounds=2")
+# so there's no point spending too much time, hence --benchmark-disable which just runs them
+# once (as a test)
 
-py.test -ra --cov=stellargraph tests/ --doctest-modules --cov-report=xml -p no:cacheprovider --junitxml="./${junit_file}" "${small_benchmarks[@]}" || exitCode=$?
+py.test -ra --cov=stellargraph tests/ --doctest-modules --cov-report=xml -p no:cacheprovider --junitxml="./${junit_file}" --benchmark-disable || exitCode=$?
 
 echo "--- :coverage::codecov::arrow_up: uploading coverage to codecov.io"
 bash <(curl https://codecov.io/bash)
