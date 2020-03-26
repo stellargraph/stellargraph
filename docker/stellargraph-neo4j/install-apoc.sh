c65cc31508a399e2723f1dc4b8df0776aa8289c8
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-03-27 09:00:34 +1100

    Don't bother with neo4j 3.4

--- docker/stellargraph-neo4j/install-apoc.sh
+++ docker/stellargraph-neo4j/install-apoc.sh
@@ -13,10 +13,6 @@ case "$NEO4J_VERSION" in
     apoc_version="3.5.0.9"
     apoc_sha="b4b1a8f8940ad250c17b45296459afcfff2eb1beee18b8c8a394e2b5a434183b924b34cb5b04fea1a371fbfb87a286228bd4ae814829e0ef99f9e190a764fa1d"
     ;;
-  3.4)
-    apoc_version="3.4.0.4"
-    apoc_sha="f84fd5d46d8d42a613e233635568e4e15b9abdabd872efb1963c6a4c8f7473744f1d345b9fdab398ac3f277f4fc2e2b4ddb84695ccdce05be4688c94075bf6ae"
-    ;;
   *)
     echo "NEO4J_VERSION: unsupported version '${NEO4J_VERSION}'"
     ;;
