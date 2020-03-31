fc66d943a4e09c2ebdee4c528f849c65bd7dbe43
PantelisElinas <pantelis.elinas@data61.csiro.au>
2020-03-31 15:40:16 +1100

    Remove classes.

--- tests/layer/test_gcn_supervised_graph_classification.py
+++ tests/layer/test_gcn_supervised_graph_classification.py
@@ -19,100 +19,101 @@ import pytest
 from ..test_utils.graphs import example_graph_random
 
 
-class Test_GCNSupervisedGraphClassification:
+graphs = [
+    example_graph_random(feature_size=4, n_nodes=6),
+    example_graph_random(feature_size=4, n_nodes=5),
+    example_graph_random(feature_size=4, n_nodes=3),
+]
 
-    graphs = [
-        example_graph_random(feature_size=4, n_nodes=6),
-        example_graph_random(feature_size=4, n_nodes=5),
-        example_graph_random(feature_size=4, n_nodes=3),
-    ]
+generator = GraphGenerator(graphs=graphs)
 
-    generator = GraphGenerator(graphs=graphs)
 
-    def test_init(self):
+def test_init():
 
-        model = GCNSupervisedGraphClassification(
-            layer_sizes=[16], activations=["relu"], generator=self.generator
+    model = GCNSupervisedGraphClassification(
+        layer_sizes=[16], activations=["relu"], generator=generator
+    )
+
+    assert len(model.layer_sizes) == 1
+    assert len(model.activations) == 1
+    assert model.layer_sizes[0] == 16
+    assert model.activations[0] == "relu"
+
+    with pytest.raises(TypeError):
+        GCNSupervisedGraphClassification(
+            layer_sizes=[16], activations=["relu"], generator=None
         )
 
-        assert len(model.layer_sizes) == 1
-        assert len(model.activations) == 1
-        assert model.layer_sizes[0] == 16
-        assert model.activations[0] == "relu"
-
-        with pytest.raises(TypeError):
-            GCNSupervisedGraphClassification(
-                layer_sizes=[16], activations=["relu"], generator=None
-            )
-
-        with pytest.raises(TypeError):
-            GCNSupervisedGraphClassification(
-                layer_sizes=[16],
-                activations=["relu"],
-                generator=FullBatchNodeGenerator(self.graphs[0]),
-            )
-
-        with pytest.raises(ValueError):
-            GCNSupervisedGraphClassification(
-                layer_sizes=[16, 32], activations=["relu"], generator=self.generator
-            )
-
-        with pytest.raises(ValueError):
-            GCNSupervisedGraphClassification(
-                layer_sizes=[32], activations=["relu", "elu"], generator=self.generator
-            )
-
-    def test_in_out_tensors(self):
-        layer_sizes = [16, 8]
-        activations = ["relu", "relu"]
-
-        model = GCNSupervisedGraphClassification(
-            layer_sizes=layer_sizes, activations=activations, generator=self.generator
+    with pytest.raises(TypeError):
+        GCNSupervisedGraphClassification(
+            layer_sizes=[16],
+            activations=["relu"],
+            generator=FullBatchNodeGenerator(graphs[0]),
         )
 
-        x_in, x_out = model.in_out_tensors()
-
-        assert len(x_in) == 3
-        assert len(x_in[0].shape) == 3
-        assert x_in[0].shape[-1] == 4  # the node feature dimensionality
-        assert len(x_out.shape) == 2
-        assert x_out.shape[-1] == layer_sizes[-1]
-
-    def test_stateful(self):
-        layer_sizes = [16, 2]
-        activations = ["elu", "elu"]
-        targets = np.array([[0, 1], [0, 1], [1, 0]])
-        train_graphs = [0, 1, 2]
-
-        gcn_graph_model = GCNSupervisedGraphClassification(
-            generator=self.generator, activations=activations, layer_sizes=layer_sizes
+    with pytest.raises(ValueError):
+        GCNSupervisedGraphClassification(
+            layer_sizes=[16, 32], activations=["relu"], generator=generator
         )
 
-        train_gen = self.generator.flow(graph_ilocs=train_graphs, targets=targets)
+    with pytest.raises(ValueError):
+        GCNSupervisedGraphClassification(
+            layer_sizes=[32], activations=["relu", "elu"], generator=generator
+        )
 
-        model_1 = tf.keras.Model(*gcn_graph_model.in_out_tensors())
-        model_2 = tf.keras.Model(*gcn_graph_model.in_out_tensors())
 
-        # check embeddings are equal before training
-        embeddings_1 = model_1.predict(train_gen)
-        embeddings_2 = model_2.predict(train_gen)
+def test_in_out_tensors():
+    layer_sizes = [16, 8]
+    activations = ["relu", "relu"]
 
-        assert np.array_equal(embeddings_1, embeddings_2)
+    model = GCNSupervisedGraphClassification(
+        layer_sizes=layer_sizes, activations=activations, generator=generator
+    )
 
-        model_1.compile(loss=tf.nn.softmax_cross_entropy_with_logits, optimizer="Adam")
-        model_1.fit(train_gen)
+    x_in, x_out = model.in_out_tensors()
 
-        # check embeddings are still equal after training one model
-        embeddings_1 = model_1.predict(train_gen)
-        embeddings_2 = model_2.predict(train_gen)
+    assert len(x_in) == 3
+    assert len(x_in[0].shape) == 3
+    assert x_in[0].shape[-1] == 4  # the node feature dimensionality
+    assert len(x_out.shape) == 2
+    assert x_out.shape[-1] == layer_sizes[-1]
 
-        assert np.array_equal(embeddings_1, embeddings_2)
 
-        model_2.compile(loss=tf.nn.softmax_cross_entropy_with_logits, optimizer="Adam")
-        model_2.fit(train_gen)
+def test_stateful():
+    layer_sizes = [16, 2]
+    activations = ["elu", "elu"]
+    targets = np.array([[0, 1], [0, 1], [1, 0]])
+    train_graphs = [0, 1, 2]
 
-        # check embeddings are still equal after training both models
-        embeddings_1 = model_1.predict(train_gen)
-        embeddings_2 = model_2.predict(train_gen)
+    gcn_graph_model = GCNSupervisedGraphClassification(
+        generator=generator, activations=activations, layer_sizes=layer_sizes
+    )
 
-        assert np.array_equal(embeddings_1, embeddings_2)
+    train_gen = generator.flow(graph_ilocs=train_graphs, targets=targets)
+
+    model_1 = tf.keras.Model(*gcn_graph_model.in_out_tensors())
+    model_2 = tf.keras.Model(*gcn_graph_model.in_out_tensors())
+
+    # check embeddings are equal before training
+    embeddings_1 = model_1.predict(train_gen)
+    embeddings_2 = model_2.predict(train_gen)
+
+    assert np.array_equal(embeddings_1, embeddings_2)
+
+    model_1.compile(loss=tf.nn.softmax_cross_entropy_with_logits, optimizer="Adam")
+    model_1.fit(train_gen)
+
+    # check embeddings are still equal after training one model
+    embeddings_1 = model_1.predict(train_gen)
+    embeddings_2 = model_2.predict(train_gen)
+
+    assert np.array_equal(embeddings_1, embeddings_2)
+
+    model_2.compile(loss=tf.nn.softmax_cross_entropy_with_logits, optimizer="Adam")
+    model_2.fit(train_gen)
+
+    # check embeddings are still equal after training both models
+    embeddings_1 = model_1.predict(train_gen)
+    embeddings_2 = model_2.predict(train_gen)
+
+    assert np.array_equal(embeddings_1, embeddings_2)
--- tests/mapper/test_graph_generator.py
+++ tests/mapper/test_graph_generator.py
@@ -20,119 +20,123 @@ import numpy as np
 import pytest
 from ..test_utils.graphs import example_graph_random, example_hin_1
 
+graphs = [
+    example_graph_random(feature_size=4, n_nodes=6),
+    example_graph_random(feature_size=4, n_nodes=5),
+    example_graph_random(feature_size=4, n_nodes=3),
+]
 
-class Test_GraphGenerator:
+graphs_nx = [
+    example_graph_random(feature_size=4, n_nodes=3, is_directed=False),
+    example_graph_random(feature_size=4, n_nodes=2, is_directed=False).to_networkx(),
+]
 
-    graphs = [
-        example_graph_random(feature_size=4, n_nodes=6),
-        example_graph_random(feature_size=4, n_nodes=5),
-        example_graph_random(feature_size=4, n_nodes=3),
-    ]
+graphs_diff_num_features = [
+    example_graph_random(feature_size=2, n_nodes=6),
+    example_graph_random(feature_size=4, n_nodes=5),
+]
 
-    graphs_nx = [
-        example_graph_random(feature_size=4, n_nodes=3, is_directed=False),
-        example_graph_random(
-            feature_size=4, n_nodes=2, is_directed=False
-        ).to_networkx(),
-    ]
+graphs_mixed = [
+    example_graph_random(feature_size=2, n_nodes=6),
+    example_hin_1(is_directed=False),
+]
 
-    graphs_diff_num_features = [
-        example_graph_random(feature_size=2, n_nodes=6),
-        example_graph_random(feature_size=4, n_nodes=5),
-    ]
 
-    graphs_mixed = [
-        example_graph_random(feature_size=2, n_nodes=6),
-        example_hin_1(is_directed=False),
-    ]
+def test_generator_init():
+    generator = GraphGenerator(graphs=graphs)
+    assert len(generator.graphs) == len(graphs)
 
-    def test_generator_init(self):
-        generator = GraphGenerator(graphs=self.graphs)
-        assert len(generator.graphs) == len(self.graphs)
 
-    def test_generator_init_different_feature_numbers(self):
-        with pytest.raises(ValueError):
-            generator = GraphGenerator(graphs=self.graphs_diff_num_features)
+def test_generator_init_different_feature_numbers():
+    with pytest.raises(ValueError):
+        generator = GraphGenerator(graphs=graphs_diff_num_features)
 
-    def test_generator_init_nx_graph(self):
-        with pytest.raises(TypeError):
-            generator = GraphGenerator(graphs=self.graphs_nx)
 
-    def test_generator_init_hin(self):
-        with pytest.raises(ValueError):
-            generator = GraphGenerator(graphs=self.graphs_mixed)
+def test_generator_init_nx_graph():
+    with pytest.raises(TypeError):
+        generator = GraphGenerator(graphs=graphs_nx)
 
-    def test_generator_flow_invalid_batch_size(self):
-        with pytest.raises(ValueError):
-            GraphGenerator(graphs=self.graphs).flow(graph_ilocs=[0], batch_size=-1)
 
-        with pytest.raises(TypeError):
-            GraphGenerator(graphs=self.graphs).flow(graph_ilocs=[0], batch_size=2.0)
+def test_generator_init_hin():
+    with pytest.raises(ValueError):
+        generator = GraphGenerator(graphs=graphs_mixed)
 
-        with pytest.raises(ValueError):
-            GraphGenerator(graphs=self.graphs).flow(graph_ilocs=[0], batch_size=0)
 
-    def test_generator_flow_incorrect_targets(self):
+def test_generator_flow_invalid_batch_size():
+    with pytest.raises(ValueError):
+        GraphGenerator(graphs=graphs).flow(graph_ilocs=[0], batch_size=-1)
 
-        generator = GraphGenerator(graphs=self.graphs)
+    with pytest.raises(TypeError):
+        GraphGenerator(graphs=graphs).flow(graph_ilocs=[0], batch_size=2.0)
 
-        with pytest.raises(ValueError):
-            generator.flow(graph_ilocs=[0, 1], targets=np.array([0]))
+    with pytest.raises(ValueError):
+        GraphGenerator(graphs=graphs).flow(graph_ilocs=[0], batch_size=0)
 
-        with pytest.raises(TypeError):
-            generator.flow(graph_ilocs=[0, 1], targets=1)
 
-    def test_generator_flow_no_targets(self):
+def test_generator_flow_incorrect_targets():
 
-        generator = GraphGenerator(graphs=self.graphs)
+    generator = GraphGenerator(graphs=graphs)
 
-        seq = generator.flow(graph_ilocs=[0, 1, 2], batch_size=2)
-        assert isinstance(seq, GraphSequence)
+    with pytest.raises(ValueError):
+        generator.flow(graph_ilocs=[0, 1], targets=np.array([0]))
 
-        assert len(seq) == 2  # two batches
+    with pytest.raises(TypeError):
+        generator.flow(graph_ilocs=[0, 1], targets=1)
 
-        # The first batch should be size 2 and the second batch size 1
-        batch_0 = seq[0]
-        assert batch_0[0][0].shape[0] == 2
-        assert batch_0[0][1].shape[0] == 2
-        assert batch_0[0][2].shape[0] == 2
-        assert batch_0[1] is None
 
-        batch_1 = seq[1]
-        assert batch_1[0][0].shape[0] == 1
-        assert batch_1[0][1].shape[0] == 1
-        assert batch_1[0][2].shape[0] == 1
-        assert batch_1[1] is None
+def test_generator_flow_no_targets():
 
-    def test_generator_flow_check_padding(self):
+    generator = GraphGenerator(graphs=graphs)
 
-        generator = GraphGenerator(graphs=self.graphs)
+    seq = generator.flow(graph_ilocs=[0, 1, 2], batch_size=2)
+    assert isinstance(seq, GraphSequence)
 
-        seq = generator.flow(graph_ilocs=[0, 2], batch_size=2)
-        assert isinstance(seq, GraphSequence)
+    assert len(seq) == 2  # two batches
 
-        assert len(seq) == 1
+    # The first batch should be size 2 and the second batch size 1
+    batch_0 = seq[0]
+    assert batch_0[0][0].shape[0] == 2
+    assert batch_0[0][1].shape[0] == 2
+    assert batch_0[0][2].shape[0] == 2
+    assert batch_0[1] is None
 
-        # The largest graph has 6 nodes vs 3 for the smallest one.
-        # Check that the data matrices have the correct size 6
-        batch = seq[0]
+    batch_1 = seq[1]
+    assert batch_1[0][0].shape[0] == 1
+    assert batch_1[0][1].shape[0] == 1
+    assert batch_1[0][2].shape[0] == 1
+    assert batch_1[1] is None
 
-        assert batch[0][0].shape[1] == 6
-        assert batch[0][1].shape[1] == 6
-        assert batch[0][2].shape[1] == 6
 
-        for mask in batch[0][1]:
-            assert np.sum(mask) == 6 or np.sum(mask) == 3
+def test_generator_flow_check_padding():
 
-    def test_generator_flow_with_targets(self):
+    generator = GraphGenerator(graphs=graphs)
 
-        generator = GraphGenerator(graphs=self.graphs)
+    seq = generator.flow(graph_ilocs=[0, 2], batch_size=2)
+    assert isinstance(seq, GraphSequence)
 
-        seq = generator.flow(graph_ilocs=[1, 2], targets=np.array([0, 1]), batch_size=1)
-        assert isinstance(seq, GraphSequence)
+    assert len(seq) == 1
 
-        for batch in seq:
-            assert batch[0][0].shape[0] == 1
-            assert batch[0][1].shape[0] == 1
-            assert batch[0][2].shape[0] == 1
-            assert batch[1].shape[0] == 1
+    # The largest graph has 6 nodes vs 3 for the smallest one.
+    # Check that the data matrices have the correct size 6
+    batch = seq[0]
+
+    assert batch[0][0].shape[1] == 6
+    assert batch[0][1].shape[1] == 6
+    assert batch[0][2].shape[1] == 6
+
+    for mask in batch[0][1]:
+        assert np.sum(mask) == 6 or np.sum(mask) == 3
+
+
+def test_generator_flow_with_targets():
+
+    generator = GraphGenerator(graphs=graphs)
+
+    seq = generator.flow(graph_ilocs=[1, 2], targets=np.array([0, 1]), batch_size=1)
+    assert isinstance(seq, GraphSequence)
+
+    for batch in seq:
+        assert batch[0][0].shape[0] == 1
+        assert batch[0][1].shape[0] == 1
+        assert batch[0][2].shape[0] == 1
+        assert batch[1].shape[0] == 1
