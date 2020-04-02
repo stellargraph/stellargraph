c727935bb4237f6b231c7f099106944aefc62c8d
pit070 <pitman.tim@gmail.com>
2020-04-02 15:10:14 +1100

    simplify comments

--- .buildkite/steps/conda-build.sh
+++ .buildkite/steps/conda-build.sh
@@ -6,9 +6,7 @@ echo "+++ :snake: :construction_worker: conda build"
 conda build . --no-anaconda-upload || exitCode=$?
 # if successful, then upload to buildkite
 if [ "${exitCode}" == "0" ]; then
-  # get the name of the resulting package
   conda_package="$(conda build . --output)"
-  # upload to buildkite
   buildkite-agent artifact upload "${conda_package}"
 fi
 
