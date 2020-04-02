8605f824b9b0037f4963f846c6f01bdadde4c72f
pit070 <pitman.tim@gmail.com>
2020-04-02 15:09:13 +1100

    bk upload

--- .buildkite/steps/conda-build.sh
+++ .buildkite/steps/conda-build.sh
@@ -3,7 +3,13 @@
 set -xeo pipefail
 
 echo "+++ :snake: :construction_worker: conda build"
-conda build . --no-anaconda-upload
-conda_package="$(conda build . --output)"
-# upload to buildkite
-buildkite-agent artifact upload "${conda_package}"
+conda build . --no-anaconda-upload || exitCode=$?
+# if successful, then upload to buildkite
+if [ "${exitCode}" == "0" ]; then
+  # get the name of the resulting package
+  conda_package="$(conda build . --output)"
+  # upload to buildkite
+  buildkite-agent artifact upload "${conda_package}"
+fi
+
+exit $exitCode
