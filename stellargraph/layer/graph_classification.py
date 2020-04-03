dcbfd5b3e52767fe13987d87a4c23e51f1772ea8
PantelisElinas <pantelis.elinas@data61.csiro.au>
2020-04-03 13:02:17 +1100

    Added DGCNN model class for supervised graph classification.

--- stellargraph/layer/graph_classification.py
+++ stellargraph/layer/graph_classification.py
@@ -18,19 +18,12 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import backend as K
 from tensorflow.keras import activations, initializers, constraints, regularizers
-from tensorflow.keras.layers import (
-    Input,
-    Layer,
-    Lambda,
-    Dropout,
-    Reshape,
-    Masking,
-    GlobalAveragePooling1D,
-)
+from tensorflow.keras.layers import Input, Layer, Lambda, Dropout, Reshape
 
 from .misc import deprecated_model_function
 from ..mapper import GraphGenerator
 from .cluster_gcn import ClusterGraphConvolution
+from .sort_pooling import SortPooling
 
 
 class GraphClassificationConvolution(ClusterGraphConvolution):
@@ -239,6 +232,189 @@ class GCNSupervisedGraphClassification:
             tuple: `(x_inp, x_out)`, where `x_inp` is a list of two input tensors for the
             Graph Classification model (containing node features and normalized adjacency matrix),
             and `x_out` is a tensor for the Graph Classification model output.
+        """
+        x_t = Input(shape=(None, self.generator.node_features_size))
+        mask = Input(shape=(None,), dtype=tf.bool)
+        A_m = Input(shape=(None, None))
+
+        x_inp = [x_t, mask, A_m]
+        x_out = self(x_inp)
+
+        return x_inp, x_out
+
+    build = deprecated_model_function(in_out_tensors, "build")
+
+
+###
+
+
+class DeepGraphConvolutionalNeuralNetwork:
+    """
+    A stack of :class:`GraphClassificationConvolution` layers together with a `SortPooling` layer
+    that implement a supervised graph classification network (DGCNN) using the GCN convolution operator
+    (https://arxiv.org/abs/1609.02907).
+
+    The DGCNN model was introduced in the paper, "An End-to-End Deep Learning Architecture for Graph Classification" by
+    M. Zhang, Z. Cui, M. Neumann, and Y. Chen, AAAI 2018, https://www.cse.wustl.edu/~muhan/papers/AAAI_2018_DGCNN.pdf
+
+    The model minimally requires specification of the GCN layer sizes as a list of ints
+    corresponding to the feature dimensions for each hidden layer,
+    activation functions for each hidden layers, and a generator object.
+
+    To use this class as a Keras model, the features and pre-processed adjacency matrix
+    should be supplied using the :class:`GraphGenerator` class.
+
+    Examples:
+        Creating a DGCNN model from a list of :class:`StellarGraph`
+        objects (``graphs``). We also add two fully connected dense layers using the last one for binary classification
+        with `softmax` activation::
+
+            generator = GraphGenerator(graphs)
+            model = DeepGraphConvolutionalNeuralNetwork(
+                             layer_sizes=[32, 32],
+                             activations=["elu","elu"],
+                             k=5,
+                             generator=generator,
+                             dropout=0.5
+                )
+            x_inp, x_out = model.in_out_tensors()
+            # Need 1-D CNN layer here
+            predictions = Dense(units=8, activation='relu')(x_out)
+            predictions = Dense(units=2, activation='softmax')(predictions)
+
+    Args:
+        layer_sizes (list of int): list of output sizes of the graph GCN layers in the stack.
+        activations (list of str): list of activations applied to each GCN layer's output.
+        k (int): size (number of rows) of output tensor.
+        generator (GraphGenerator): an instance of :class:`GraphGenerator` class constructed on the graphs used for
+            training.
+        bias (bool, optional): toggles an optional bias in graph convolutional layers.
+        dropout (float, optional): dropout rate applied to input features of each GCN layer.
+        kernel_initializer (str or func, optional): The initialiser to use for the weights of each graph
+            convolutional layer.
+        kernel_regularizer (str or func, optional): The regulariser to use for the weights of each graph
+            convolutional layer.
+        kernel_constraint (str or func, optional): The constraint to use for the weights of each layer graph
+            convolutional.
+        bias_initializer (str or func, optional): The initialiser to use for the bias of each layer graph
+            convolutional.
+        bias_regularizer (str or func, optional): The regulariser to use for the bias of each layer graph
+            convolutional.
+        bias_constraint (str or func, optional): The constraint to use for the bias of each layer graph
+            convolutional.
+
+    """
+
+    def __init__(
+        self,
+        layer_sizes,
+        activations,
+        k,
+        generator,
+        bias=True,
+        dropout=0.0,
+        kernel_initializer=None,
+        kernel_regularizer=None,
+        kernel_constraint=None,
+        bias_initializer=None,
+        bias_regularizer=None,
+        bias_constraint=None,
+    ):
+        if not isinstance(generator, GraphGenerator):
+            raise TypeError(
+                f"generator: expected instance of GraphGenerator, found {type(generator).__name__}"
+            )
+
+        if len(layer_sizes) != len(activations):
+            raise ValueError(
+                "expected the number of layers to be the same as the number of activations,"
+                f"found {len(layer_sizes)} layer sizes vs {len(activations)} activations"
+            )
+
+        if not isinstance(k, int):
+            raise TypeError(
+                f"k: expected k to be integer type, found {type(k).__name}."
+            )
+
+        if k <= 0:
+            raise ValueError(f"k: expected k to be strictly positive, found {k}")
+
+        self.layer_sizes = layer_sizes
+        self.activations = activations
+        self.k = k
+        self.bias = bias
+        self.dropout = dropout
+        self.generator = generator
+
+        # Initialize a stack of GraphClassificationConvolution layers
+        n_layers = len(self.layer_sizes)
+        self._layers = []
+        for ii in range(n_layers):
+            l = self.layer_sizes[ii]
+            a = self.activations[ii]
+            self._layers.append(Dropout(self.dropout))
+            self._layers.append(
+                GraphClassificationConvolution(
+                    l,
+                    activation=a,
+                    use_bias=self.bias,
+                    kernel_initializer=kernel_initializer,
+                    kernel_regularizer=kernel_regularizer,
+                    kernel_constraint=kernel_constraint,
+                    bias_initializer=bias_initializer,
+                    bias_regularizer=bias_regularizer,
+                    bias_constraint=bias_constraint,
+                )
+            )
+
+        # And the SortPooling layer
+        self._layers.append(SortPooling(k=self.k))
+
+    def __call__(self, x):
+        """
+        Apply a stack of :class:`GraphClassificationConvolution` layers to the inputs followed by a single
+        SortPooling layer.
+        The input tensors are expected to be a list of the following:
+        [
+            Node features shape (batch size, N, F),
+            Mask (batch size, N ),
+            Adjacency matrices (batch size, N, N),
+        ]
+        where N is the number of nodes and F the number of input features
+
+        Args:
+            x (Tensor): input tensors
+
+        Returns:
+            Output tensor
+        """
+        gcn_layers = []
+
+        x_in, mask, As = x
+        h_layer = x_in
+
+        for layer in self._layers:
+            if isinstance(layer, GraphClassificationConvolution):
+                h_layer = layer([h_layer, As])
+                gcn_layers.append(h_layer)
+            elif isinstance(layer, SortPooling):
+                # concatenate the GCN output tensors and use as input to the SortPooling layer
+                h_layer = tf.concat(gcn_layers, axis=-1)
+                h_layer = layer(h_layer)
+            else:
+                # For other (non-graph) layers only supply the input tensor
+                h_layer = layer(h_layer)
+
+        return h_layer
+
+    def in_out_tensors(self):
+        """
+        Builds a DGCNN Graph Classification model.
+
+        Returns:
+            tuple: `(x_inp, x_out)`, where `x_inp` is a list of two input tensors for the
+            Graph Classification model (containing node features and normalized adjacency matrix),
+            and `x_out` is a tensor for the DGCNN Graph Classification model output.
         """
         x_t = Input(shape=(None, self.generator.node_features_size))
         mask = Input(shape=(None,), dtype=tf.bool)
