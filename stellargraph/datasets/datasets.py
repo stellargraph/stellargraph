f5888e3062e2fd55ecc62d2ef7b852d663d20219
Habiba <habiba.habiba@data61.csiro.au>
2020-04-14 19:35:40 +1000

    added LA dataset class for spatio-temporal stuff

--- stellargraph/datasets/datasets.py
+++ stellargraph/datasets/datasets.py
@@ -61,11 +61,11 @@ def _load_cora_or_citeseer(
         column_names = feature_names + [subject]
 
     node_data = pd.read_csv(
-        content, sep="\t", header=None, names=column_names, dtype={0: nodes_dtype},
+        content, sep="\t", header=None, names=column_names, dtype={0: nodes_dtype}
     )
 
     edgelist = pd.read_csv(
-        cites, sep="\t", header=None, names=["target", "source"], dtype=nodes_dtype,
+        cites, sep="\t", header=None, names=["target", "source"], dtype=nodes_dtype
     )
 
     valid_source = node_data.index.get_indexer(edgelist.source) >= 0
@@ -803,3 +803,54 @@ class IAEnronEmployees(
         )
 
         return StellarGraph(nodes=nodes, edges=edges, edge_weight_column="time"), edges
+
+
+class METR_LA(
+    DatasetLoader,
+    name="METR-LA",
+    directory_name="METR-LA",
+    url="https://github.com/lehaifeng/T-GCN/tree/master/data",
+    url_archive_format="n/a",
+    expected_files=["los_speed.csv", "los_adj.csv"],
+    description="This traffic dataset contains traffic information collected from loop detectors in the highway of Los Angeles County (Jagadish et al., 2014).",
+    source="https://github.com/lehaifeng/T-GCN/tree/master/data",
+):
+    def load(self):
+        los_adj = pd.read_csv(r"data/los_adj.csv", header=None)
+        adj = np.mat(los_adj)
+        los_tf = pd.read_csv(r"data/los_speed.csv")
+        return los_tf, adj
+
+    def train_test_split(self, data, train_portion):
+        time_len = data.shape[0]
+        train_size = int(time_len * train_portion)
+        train_data = np.array(data[:train_size])
+        test_data = np.array(data[train_size:])
+        return train_data, test_data
+
+    def scale_data(self, train_data, test_data):
+        max_speed = train_data.max()
+        min_speed = train_data.min()
+        train_scaled = (train_data - min_speed) / (max_speed - min_speed)
+        test_scaled = (test_data - min_speed) / (max_speed - min_speed)
+        return train_scaled, test_scaled
+
+    def sequence_data_preparation(self, seq_len, pre_len, train_data, test_data):
+        trainX, trainY, testX, testY = [], [], [], []
+
+        for i in range(len(train_data) - int(seq_len + pre_len - 1)):
+            a = train_data[i : i + seq_len + pre_len,]
+            trainX.append(a[:seq_len])
+            trainY.append(a[-1])
+
+        for i in range(len(test_data) - int(seq_len + pre_len - 1)):
+            b = test_data[i : i + seq_len + pre_len,]
+            testX.append(b[:seq_len,])
+            testY.append(b[-1])
+
+        trainX = np.array(trainX)
+        trainY = np.array(trainY)
+        testX = np.array(testX)
+        testY = np.array(testY)
+
+        return trainX, trainY, testX, testY
