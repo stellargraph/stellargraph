4f806abddb6e27d6ea187ee09e0a74911e66164d
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-04-16 11:51:44 +1000

    Add a test that we're using a GPU

---
+++ tests/test_on_gpu.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020 Data61, CSIRO
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import tensorflow as tf
+import pytest
+
+# When the environment variable is set, we are
+def test_on_gpu_when_requested():
+    if os.environ.get("STELLARGRAPH_MUST_USE_GPU") != "1":
+        pytest.skip(
+            "STELLARGRAPH_MUST_USE_GPU is not set to 1, so a GPU does not have to be used"
+        )
+
+    # we can't easily capture this
+    tf.debugging.set_log_device_placement(True)
+
+    # check we can execute something on it
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name="a")
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name="b")
+    c = tf.matmul(a, b)
+
+    assert c.numpy().shape == (2, 2)
+
+    assert tf.config.list_physical_devices("GPU")
+    # FIXME: remove this, just to show the logging
+    assert False
