142ec162a26010dc9916cd9407875c3c379e484b
PantelisElinas <pantelis.elinas@data61.csiro.au>
2020-04-17 16:28:39 +1000

    Remove duplicate code in test.

--- tests/mapper/test_padded_graph_generator.py
+++ tests/mapper/test_padded_graph_generator.py
@@ -165,29 +165,24 @@ def test_generator_flow_with_targets():
         assert batch[1].shape[0] == 1
 
 
-def test_generator_adj_normalisation():
+@pytest.mark.parametrize("symmetric_normalization", [True, False])
+def test_generator_adj_normalisation(symmetric_normalization):
 
     graph = example_graph(feature_size=4)
 
     generator = PaddedGraphGenerator(graphs=[graph])
-
-    seq = generator.flow(graph_ilocs=[0], symmetric_normalization=False)
-
-    adj_norm_seq = seq.normalized_adjs[0].todense()
-
-    adj = np.array(graph.to_adjacency_matrix().todense())
-    inv_deg = np.diag(1.0 / adj.sum(axis=1))
-    adj_norm = inv_deg.dot(adj)
-
-    assert np.allclose(adj_norm_seq, adj_norm)
-
-    seq = generator.flow(graph_ilocs=[0], symmetric_normalization=True)
+    seq = generator.flow(
+        graph_ilocs=[0], symmetric_normalization=symmetric_normalization
+    )
 
     adj_norm_seq = seq.normalized_adjs[0].todense()
 
     adj = np.array(graph.to_adjacency_matrix().todense())
-    inv_deg = np.diag(np.sqrt(1.0 / adj.sum(axis=1)))
-
-    adj_norm = inv_deg.dot(adj).dot(inv_deg)
+    if symmetric_normalization:
+        inv_deg = np.diag(np.sqrt(1.0 / adj.sum(axis=1)))
+        adj_norm = inv_deg.dot(adj).dot(inv_deg)
+    else:
+        inv_deg = np.diag(1.0 / adj.sum(axis=1))
+        adj_norm = inv_deg.dot(adj)
 
     assert np.allclose(adj_norm_seq, adj_norm)
