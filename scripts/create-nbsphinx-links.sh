c4ccd1962008cc1c489fa2e38bddc3d9aaf6e783
kevin <kevin.jung@data61.csiro.au>
2020-04-20 16:56:23 +1000

    Add script

---
+++ scripts/create-nbsphinx-links.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# get all notebooks
+for nb_path in $(find demos -name "*.ipynb"); do
+  nb_link="docs/${nb_path%.ipynb}.nblink"
+  nb_link_dir="$(dirname "$nb_link")"
+
+  # recurse through parent directories and create index.txt
+  while [ "$nb_link_dir" != "docs" ]; do
+    nb_link_parent_index="$nb_link_dir/index.txt"
+    if [[ ! -f "$nb_link_parent_index" ]]; then
+      echo "creating $nb_link_parent_index"
+      mkdir -p "$nb_link_dir"
+      cat > "$nb_link_parent_index" << EOF
+$(basename "$nb_link_dir")
+====================================
+
+.. toctree::
+    :titlesonly:
+    :glob:
+
+    */index
+    ./*
+EOF
+    else
+      break
+    fi
+    nb_link_dir="$(dirname "$nb_link_dir")"
+  done
+
+  # create .nblink file
+  nb_link_dir="$(dirname "$nb_link")"
+  link_relative_path="$(realpath --relative-to="$nb_link_dir" "$nb_path")"
+  cat > "$nb_link" << EOF
+{
+  "path": "$link_relative_path"
+}
+EOF
+done
