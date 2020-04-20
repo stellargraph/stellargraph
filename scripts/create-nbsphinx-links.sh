a79396643b4d7f4f4649629c446cc6209190f4b0
kevin <kevin.jung@data61.csiro.au>
2020-04-20 17:00:54 +1000

    Update script

--- scripts/create-nbsphinx-links.sh
+++ scripts/create-nbsphinx-links.sh
@@ -3,13 +3,13 @@
 # get all notebooks
 for nb_path in $(find demos -name "*.ipynb"); do
   nb_link="docs/${nb_path%.ipynb}.nblink"
-  nb_link_dir="$(dirname "$nb_link")"
 
   # recurse through parent directories and create index.txt
+  nb_link_dir="$(dirname "$nb_link")"
   while [ "$nb_link_dir" != "docs" ]; do
     nb_link_parent_index="$nb_link_dir/index.txt"
     if [[ ! -f "$nb_link_parent_index" ]]; then
-      echo "creating $nb_link_parent_index"
+      echo "Creating '$nb_link_parent_index'"
       mkdir -p "$nb_link_dir"
       cat > "$nb_link_parent_index" << EOF
 $(basename "$nb_link_dir")
@@ -29,6 +29,7 @@ EOF
   done
 
   # create .nblink file
+  echo "Creating '$nb_link'"
   nb_link_dir="$(dirname "$nb_link")"
   link_relative_path="$(realpath --relative-to="$nb_link_dir" "$nb_path")"
   cat > "$nb_link" << EOF
