947ee609d7ccb1a35b261b9054582e84293f7bc9
kieranricardo <kieranricardo@hotmail.com>
2020-04-24 12:38:04 +1000

    revert random walks bacl to returning lists of lists

--- stellargraph/data/explorer.py
+++ stellargraph/data/explorer.py
@@ -292,7 +292,7 @@ class UniformRandomWalk(RandomWalk):
                 current_node = rs.choice(neighbours)
             walk.append(current_node)
 
-        return self.graph.node_ilocs_to_ids(walk)
+        return list(self.graph.node_ilocs_to_ids(walk))
 
 
 def naive_weighted_choices(rs, weights):
@@ -472,7 +472,7 @@ class BiasedRandomWalk(RandomWalk):
                         previous_node_neighbours = neighbours
                         current_node = neighbours[choice]
 
-                walks.append(self.graph.node_ilocs_to_ids(walk))
+                walks.append(list(self.graph.node_ilocs_to_ids(walk)))
 
         return walks
 
@@ -588,7 +588,9 @@ class UniformRandomMetaPathWalk(RandomWalk):
                             neighbours
                         )  # the next node in the walk
 
-                    walks.append(self.graph.node_ilocs_to_ids(walk))  # store the walk
+                    walks.append(
+                        list(self.graph.node_ilocs_to_ids(walk))
+                    )  # store the walk
 
         return walks
 
