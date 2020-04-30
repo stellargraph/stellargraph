2b8453e7b5483810475e75c1dfb987adef095dfe
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-04-30 14:34:57 +1000

    Use commonmark library for validating notebook text, not regexes (#1393)
    This is a building block for #1292, since part of that will require validating
    the contents of link elements (see: #1394), which will be much more reliable
    with a real parser.
    
    I chose the commonmark library https://github.com/readthedocs/commonmark.py
    because it (a) provides easy access to an AST node type, and (b) that AST
    records the source positions of block elements (in the form of the `sourcepos`
    property, which is a list like `[start, end]`, where `start` and `end` are lists
    like `[line_number, column_number]` (one-based)). The source positions make it
    easy to report nice error messages with lines from the cells, without having to
    reconstruct/guess at which part of the code was broken.
    
    This does increase the amount of code. Most of that is in support code, while
    the `@checker`s themselves and even error message construction get (arguably)
    simpler. The code can now reason in terms of actual markdown entities, instead
    of having to do regex parsing/guessing.
    
    This also catches one instance of a heading that the regexes did not, in the
    PPNP demo.
    
    Validation: I added some broken-on-purpose notebooks and built them in:
    https://buildkite.com/stellar/stellargraph-public/builds/3541
    
    See: #1292

--- scripts/notebook_text_checker.py
+++ scripts/notebook_text_checker.py
@@ -17,6 +17,7 @@
 # limitations under the License.
 
 import argparse
+import commonmark
 import nbformat
 import os
 import re
@@ -25,49 +26,138 @@ import sys
 import textwrap
 from pathlib import Path
 
-# helpers
+
+class FormattingError(Exception):
+    """
+    A checker that finds error(s) should raise this with all the errors.
+    """
+
+    def __init__(self, errors):
+        if not isinstance(errors, list):
+            errors = [errors]
+
+        self.errors = errors
+
+
+# notebook and cell helpers
 
 
 def cell_source(cell):
     return "".join(cell.source)
 
 
-def cell_lines(cell, start, end):
-    """
-    Return the lines from `cell` that contain the range 'start:end' (for instance, a return value of `find`).
-    """
-    if start > end:
-        raise ValueError(f"start ({start}) > end ({end})")
-
-    source = cell_source(cell)
-    start_line = source.rfind("\n", 0, start)
-    end_line = source.find("\n", end)
-    if end_line == -1:
-        end_line = len(source)
-
-    # 1-based line number of start of the section of lines
-    start_num = source.count("\n", 0, start) + 1
-
-    # we don't want to include either of the \n's: 'end' is already sliced exclusively, but 'start'
-    # needs tweaking (note that start == -1 works correctly: that case needs to slice from the start
-    # of the string, and it ends up as source[0:end], as desired)
-    text = source[start_line + 1 : end_line]
-
-    return start_num, text
-
-
-def message_with_line(cell, message, start=0, end=None):
-    if end is None:
-        end = start
-
-    start_num, lines = cell_lines(cell, start, end)
+def number_lines(lines, first_num):
     # indent the code (so it markdowns as a code block) and give it line numbers
-    indented = "".join(
+    return "\n".join(
         f"    {line_num:3} | {line}"
-        for line_num, line in enumerate(lines.splitlines(True), start=start_num)
+        for line_num, line in enumerate(lines, start=first_num)
     )
 
-    return f"{message}. Some relevant lines from the cell:\n\n{indented}"
+
+COMMONMARK_PARSER = commonmark.Parser()
+
+
+class MarkdownCell:
+    """
+    A markdown cell, that's been preprocessed (e.g. parsed into a Markdown AST).
+    """
+
+    def __init__(self, cell):
+        source = cell_source(cell)
+        self._lines = source.splitlines()
+
+        self.ast = COMMONMARK_PARSER.parse(source)
+        # make sure we don't have to deal with '<text><text>' elements
+        self.ast.normalize()
+
+    def lines(self, sourcepos):
+        """
+        Retrieve the lines corresponding to the 'sourcepos' range.
+
+        Args:
+            sourcepos: pair of pairs [[start line, start column], [end line, end column]], with one
+                based lines and columns (the same as a commonmark's Node.sourcepos property)
+        """
+        one_based_start = sourcepos[0][0]
+        # the commonmark sourcepos's are one based, so slicing needs an offset
+        start = one_based_start - 1
+        end = sourcepos[1][0] - 1
+
+        # we want to include the end line
+        lines = self._lines[start : end + 1]
+        return number_lines(lines, one_based_start)
+
+
+def parse_markdown_cells(notebook):
+    def cell(c):
+        if c.cell_type == "markdown":
+            return MarkdownCell(c)
+        return c
+
+    return [cell(c) for c in notebook.cells]
+
+
+def message_with_line(cell, message, sourcepos=[[1, 1], [1, 1]]):
+    """
+    Print 'message', along with some of the lines of 'cell'
+    """
+    return f"{message}. Some relevant lines from the cell:\n\n{cell.lines(sourcepos)}"
+
+
+## commonmark helpers
+
+
+def is_heading(elem):
+    return elem.t == "heading"
+
+
+def is_title(elem):
+    return is_heading(elem) and elem.level == 1
+
+
+def direct_children(parent):
+    """
+    Iterate over the direct children of 'parent' (not all descendants, like 'parent.walker()')
+    """
+    elem = parent.first_child
+    while elem is not None:
+        yield elem
+        elem = elem.nxt
+
+
+def index_of_first(list_, pred):
+    for i, x in enumerate(list_):
+        if pred(x):
+            return i
+
+    return None
+
+
+def surrounding_sourcepos(elems, start_index, end_index=None):
+    """
+    Find a sourcepos that ranges from the last line of elems[start_index - 1] to the first line
+    of elems[end_index + 1], handling the boundary conditions.
+    """
+    if end_index is None:
+        end_index = start_index
+
+    # chosen start element ...
+    if start_index == 0:
+        # ... is first, so start at its first line
+        start = elems[start_index].sourcepos[0]
+    else:
+        # ... isn't first, so start at the previous element's last line
+        start = elems[start_index - 1].sourcepos[1]
+
+    # chosen end element ...
+    if end_index == len(elems) - 1:
+        # ... is last, so end at its last line
+        end = elems[end_index].sourcepos[1]
+    else:
+        # ... isn't last, so end at the next element's first line
+        end = elems[end_index + 1].sourcepos[0]
+
+    return [start, end]
 
 
 ## checkers
@@ -81,100 +171,114 @@ def checker(f):
     return f
 
 
-NON_WHITESPACE_RE = re.compile(r"\S")
-TITLE_RE = re.compile("^# .*", re.MULTILINE)
-HEADING_RE = re.compile("^(?P<level>##*) .*", re.MULTILINE)
-
-
 @checker
-def title_heading(notebook):
+def title_heading(cells):
     """
     The first cell should be the title (and only the title), so that the "cloud runner" cell appears
     immediately after it.
     """
-    first = notebook.cells[0]
-    if first.cell_type != "markdown":
-        return [
-            message_with_line(
-                first,
-                "The first cell should be a markdown cell (containing only the title, like '# ...'). This one seems to be a code cell",
-            )
-        ]
+    first = cells[0]
+    if not isinstance(first, MarkdownCell):
+        source = cell_source(first)
+        # slice (not index) to handle an empty cell
+        first_line = source.splitlines()[:1]
+        lines = number_lines(first_line, 1)
 
-    source = cell_source(first)
+        raise FormattingError(
+            f"The first cell should be a markdown cell (containing only a title, like `# ...`). This one seems to be a code cell. First line of the cell:\n\n{lines}"
+        )
 
-    if TITLE_RE.fullmatch(source.strip()) is not None:
-        # all good, only whitespace and the # ... title.
-        return []
+    elems = list(direct_children(first.ast))
+    title_idx = index_of_first(elems, is_title)
 
-    title = TITLE_RE.search(source)
-    if title is None:
-        return [
+    if title_idx is None:
+        # no title at at all
+        raise FormattingError(
             message_with_line(
                 first,
                 "The first cell should be just the title for the notebook (like `# ...`) but the title seems to be missing here",
             )
-        ]
+        )
 
-    # report the error nicely
-    first_trailing_nonwhitespace = NON_WHITESPACE_RE.search(source, title.end())
-    if first_trailing_nonwhitespace is None:
-        end = len(source)
-    else:
-        end = first_trailing_nonwhitespace.end()
+    if len(elems) == 1:
+        # all good, only element is a title
+        return
 
-    return [
+    # have a title, but there's other things too.
+    sourcepos = surrounding_sourcepos(elems, title_idx)
+
+    raise FormattingError(
         message_with_line(
             first,
             "The first cell should contain only the title (like `# ...`) for the notebook. Additional introductory content can be in a separate following cell",
-            start=0,
-            end=end,
+            sourcepos=sourcepos,
         )
-    ]
+    )
 
 
 @checker
-def other_headings(notebook):
+def other_headings(cells):
     """
-    There should be no other H1/titles in the notebook, so that table of contents levels are correct
-    on Read the Docs.
+    No other H1/titles, and no heading level skipping.
+
+    Extra titles break tables of contents, and heading level skipping causes Sphinx/reStructuredText
+    warnings.
     """
-    previous_heading_level = 1
+    # keep track of any heading level skips, but only compare to headings that are nested correctly,
+    # so an invalid section heading gets flagged, and so do any subheadings within that section.
+    previous_valid_heading_level = 1
+    first_invalid_heading_level = None
 
     errors = []
-    for cell in notebook.cells[1:]:
-        if cell.cell_type != "markdown":
+    for cell in cells[1:]:
+        if not isinstance(cell, MarkdownCell):
             continue
 
-        source = cell_source(cell)
-        for heading in HEADING_RE.finditer(source):
-            if TITLE_RE.match(heading[0]):
+        for elem, entering in cell.ast.walker():
+            # only look at headings, and only look at them once
+            if not is_heading(elem) or not entering:
+                continue
+
+            if is_title(elem):
                 errors.append(
                     message_with_line(
                         cell,
                         "Found another title (like `# ...`) in internal cell. Later sections should use a high level heading (like `## ...` or `### ...`)",
-                        start=heading.start(),
+                        sourcepos=elem.sourcepos,
                     )
                 )
 
-            level = len(heading["level"])
-            highest_valid_level = previous_heading_level + 1
-            if level > highest_valid_level:
-                previous = "#" * previous_heading_level
+            if elem.level > previous_valid_heading_level + 1:
+                previous = "#" * previous_valid_heading_level
+
+                if first_invalid_heading_level is None:
+                    first_invalid_heading_level = elem.level
+
+                # assume that there's only one level skip (e.g. H1, H3, H3(*), H4, H3), and that the
+                # relative levels within the invalid section are correct. This means for all H3
+                # suggest only H2, but for H4 suggest H3 too (to continue nesting within (*)).
+                levels_from_first_invalid = elem.level - first_invalid_heading_level
+                max_suggestion_level = (
+                    previous_valid_heading_level + levels_from_first_invalid + 1
+                )
+
                 suggestions = ", ".join(
-                    f"`{'#' * i} ...`" for i in range(2, highest_valid_level + 1)
+                    f"`{'#' * i} ...`" for i in range(2, max_suggestion_level + 1)
                 )
                 errors.append(
                     message_with_line(
                         cell,
-                        f"Found a heading H{level} that skips level(s) from previous heading (H{previous_heading_level} `{previous} ...`). Consider using: {suggestions}",
-                        start=heading.start(),
+                        f"Found a heading H{elem.level} that skips level(s) from most recent valid heading (H{previous_valid_heading_level} `{previous} ...`). Consider using: {suggestions}",
+                        sourcepos=elem.sourcepos,
                     )
                 )
+            else:
+                # this was valid, so we can reset our counts
+                previous_valid_heading_level = elem.level
+                first_invalid_heading_level = None
 
-            previous_heading_level = level
-
-    return errors
+    if errors:
+        raise FormattingError(errors)
 
 
 # ANSI terminal escape sequences
@@ -214,15 +318,18 @@ def main():
 
         print(f"{YELLOW_BOLD}Checking file {file_loc}{RESET}")
         notebook = nbformat.read(str(file_loc), as_version=4)
+        cells = parse_markdown_cells(notebook)
 
         this_errors = []
         for checker in CHECKERS:
-            errors = checker(notebook)
-            for e in errors:
-                print(f"{LIGHT_RED_BOLD}error{RESET}: {e}\n")
-            this_errors.extend(errors)
+            try:
+                checker(cells)
+            except FormattingError as exc:
+                for e in exc.errors:
+                    print(f"{LIGHT_RED_BOLD}error{RESET}: {e}\n")
+                this_errors.extend(exc.errors)
 
-        if errors:
+        if this_errors:
             all_errors.append((file_loc, this_errors))
 
     if all_errors:
