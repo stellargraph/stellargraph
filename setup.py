1faf40905321dd3a1e274d703cde83c200007279
kieranricardo <kieranricardo@hotmail.com>
2020-05-01 16:22:47 +1000

    removed extra dependency

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ EXTRAS_REQUIRES = {
     "neo4j": ["py2neo"],
     "test": [
         "pytest==5.3.1",
-        "pytest-benchmark>=3.1",
         "pytest-cov>=2.6.0",
         "pytest-repeat>=0.8.0",
         "coverage>=4.4,<5.0",
