9b0d67dda3de5be92958ea8c6a7b1dc1f8e4231f
Jeremy Goh <jeremy_gsk@outlook.com>
2020-05-01 14:55:25 +0800

    Tidy up docstring

--- stellargraph/mapper/full_batch_generators.py
+++ stellargraph/mapper/full_batch_generators.py
@@ -157,8 +157,8 @@ class FullBatchGenerator(Generator):
         Args:
             node_ids: an iterable of node ids for the nodes of interest
                 (e.g., training, validation, or test set nodes)
-            targets: a 1D or 2D array of numeric node targets with shape `(len(node_ids)`
-                or (len(node_ids), target_size)`
+            targets: a 1D or 2D array of numeric node targets with shape `(len(node_ids),)`
+                or `(len(node_ids), target_size)`
 
         Returns:
             A NodeSequence object to use with GCN or GAT models
@@ -209,18 +209,18 @@ class FullBatchNodeGenerator(FullBatchGenerator):
     'method' option should be specified with the correct pre-processing for
     each algorithm. The options are as follows:
 
-    *   ``method='gcn'`` Normalizes the adjacency matrix for the GCN algorithm.
+    *   ``method='gcn'``: Normalizes the adjacency matrix for the GCN algorithm.
         This implements the linearized convolution of Eq. 8 in [1].
     *   ``method='sgc'``: This replicates the k-th order smoothed adjacency matrix
         to implement the Simplified Graph Convolutions of Eq. 8 in [2].
     *   ``method='self_loops'`` or ``method='gat'``: Simply sets the diagonal elements
         of the adjacency matrix to one, effectively adding self-loops to the graph. This is
         used by the GAT algorithm of [3].
-    *   ``method='ppnp'`` Calculates the personalized page rank matrix of Eq 2 in [4].
+    *   ``method='ppnp'``: Calculates the personalized page rank matrix of Eq. 2 in [4].
 
     [1] `Kipf and Welling, 2017 <https://arxiv.org/abs/1609.02907>`_.
     [2] `Wu et al. 2019 <https://arxiv.org/abs/1902.07153>`_.
-    [3] `Veličković et al., 2018 <https://arxiv.org/abs/1710.10903>`_
+    [3] `Veličković et al., 2018 <https://arxiv.org/abs/1710.10903>`_.
     [4] `Klicpera et al., 2018 <https://arxiv.org/abs/1810.05997>`_.
 
     Example::
@@ -295,18 +295,18 @@ class FullBatchLinkGenerator(FullBatchGenerator):
     'method' option should be specified with the correct pre-processing for
     each algorithm. The options are as follows:
 
-    *   ``method='gcn'`` Normalizes the adjacency matrix for the GCN algorithm.
+    *   ``method='gcn'``: Normalizes the adjacency matrix for the GCN algorithm.
         This implements the linearized convolution of Eq. 8 in [1].
     *   ``method='sgc'``: This replicates the k-th order smoothed adjacency matrix
         to implement the Simplified Graph Convolutions of Eq. 8 in [2].
     *   ``method='self_loops'`` or ``method='gat'``: Simply sets the diagonal elements
         of the adjacency matrix to one, effectively adding self-loops to the graph. This is
         used by the GAT algorithm of [3].
-    *   ``method='ppnp'`` Calculates the personalized page rank matrix of Eq 2 in [4].
+    *   ``method='ppnp'``: Calculates the personalized page rank matrix of Eq. 2 in [4].
 
     [1] `Kipf and Welling, 2017 <https://arxiv.org/abs/1609.02907>`_.
     [2] `Wu et al. 2019 <https://arxiv.org/abs/1902.07153>`_.
-    [3] `Veličković et al., 2018 <https://arxiv.org/abs/1710.10903>`_
+    [3] `Veličković et al., 2018 <https://arxiv.org/abs/1710.10903>`_.
     [4] `Klicpera et al., 2018 <https://arxiv.org/abs/1810.05997>`_.
 
     Example::
@@ -348,8 +348,8 @@ class FullBatchLinkGenerator(FullBatchGenerator):
         Args:
             link_ids: an iterable of link ids specified as tuples of node ids
                 or an array of shape (N_links, 2) specifying the links.
-            targets: a 1D or 2D array of numeric node targets with shape `(len(node_ids)`
-                or (len(node_ids), target_size)`
+            targets: a 1D or 2D array of numeric node targets with shape `(len(node_ids),)`
+                or `(len(node_ids), target_size)`
 
         Returns:
             A NodeSequence object to use with GCN or GAT models
