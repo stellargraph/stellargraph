55673f22770d704a68e117d28de2afc15da5fd6e
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-05-04 20:49:42 +1000

    Temporarily exclude community detection demo from published docs

--- docs/conf.py
+++ docs/conf.py
@@ -92,7 +92,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "requirements.txt"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "requirements.txt", "demos/community_detection/*"]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "sphinx"
