d33c650bc3223df4676d088c82d7f790c124e57a
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-05-04 10:40:37 +1000

    Disallow leading-block-quote in cells, allow summary in title cell (#1413)
    This works around problems in the Markdown to reStructuredText conversion that
    nbsphinx does when rendering notebooks. In particular, spatialaudio/nbsphinx#450
    and/or jupyter/nbconvert#1245. These issues mean that if a cell starts with a
    block quote, it will merge with a reStructuredText directive at the end of the
    previous cell, because an appropriate separating comment is not inserted.
    
    Pull request #1398 gives an example of this; the `loading-networkx.ipynb`
    notebook's rST from `nbconvert` ends up looking like:
    
    ```rst
       Run the master version of this notebook: |Open In Binder| |Open In
       Colab|
    
    .. |Open In Binder| image:: https://mybinder.org/badge_logo.svg
       :target: https://mybinder.org/v2/gh/stellargraph/stellargraph/master?urlpath=lab/tree/demos/basics/loading-networkx.ipynb
    .. |Open In Colab| image:: https://colab.research.google.com/assets/colab-badge.svg
       :target: https://colab.research.google.com/github/stellargraph/stellargraph/blob/master/demos/basics/loading-networkx.ipynb
    
       This demo explains how to load data from NetworkX into a form that
       can be used by the StellarGraph library. `See all other
       demos <../README.md>`__.
    ```
    
    The "This demo ..." text gets included as part of the `|Open In Colab|`
    substitution definition. This notebook thus isn't displayed correct, and
    potentially has warnings/errors when rendering. If this was all in a single
    cell, the rST would look like:
    
    ```rst
       Run the master version of this notebook: |Open In Binder| |Open In
       Colab|
    
    .. |Open In Binder| image:: https://mybinder.org/badge_logo.svg
       :target: https://mybinder.org/v2/gh/stellargraph/stellargraph/master?urlpath=lab/tree/demos/basics/loading-networkx.ipynb
    .. |Open In Colab| image:: https://colab.research.google.com/assets/colab-badge.svg
       :target: https://colab.research.google.com/github/stellargraph/stellargraph/blob/master/demos/basics/loading-networkx.ipynb
    
    ..
    
       This demo explains how to load data from NetworkX into a form that
       can be used by the StellarGraph library. `See all other
       demos <../README.md>`__.
    ```
    
    Where the extra `..` is an empty comment, which is the recommended way to
    separate a quote from an earlier construct
    (https://docutils.sourceforge.io/docs/ref/rst/restructuredtext.html#block-quotes).
    
    This pull request fixes this in two ways:
    
    - flagging any markdown cells that have a block quote at the start, since they
      may hit these problems (except for the cloud-runner ones, which are entirely a
      quote now #1398)
    - allowing the first cell to also include a summary after title, where the
      summary is denoted by a `> ...` quote element
    
    Examples of the failures (on the notebooks that were fixed here):
    https://buildkite.com/stellar/stellargraph-public/builds/3613
    
    Comparison: (see #1413 for images)
    
    - Before: The notebook summary is missing.
      https://stellargraph--1398.org.readthedocs.build/en/1398/demos/basics/loading-pandas.html
    - After: The notebook summary is included.
      https://stellargraph--1413.org.readthedocs.build/en/1413/demos/basics/loading-pandas.html
    
    Also see:
    https://github.com/stellargraph/stellargraph/pull/1398#issuecomment-621606354
    
    See: #1293

--- scripts/notebook_text_checker.py
+++ scripts/notebook_text_checker.py
@@ -64,6 +64,7 @@ class MarkdownCell:
 
     def __init__(self, cell):
         source = cell_source(cell)
+        self.metadata = cell.metadata
         self._lines = source.splitlines()
 
         self.ast = COMMONMARK_PARSER.parse(source)
@@ -113,6 +114,10 @@ def is_heading(elem):
 
 def is_title(elem):
     return is_heading(elem) and elem.level == 1
+
+
+def is_block_quote(elem):
+    return elem.t == "block_quote"
 
 
 def is_inline(elem):
@@ -233,7 +238,7 @@ def title_heading(cells):
         lines = number_lines(first_line, 1)
 
         raise FormattingError(
-            f"The first cell should be a markdown cell (containing only a title, like `# ...`). This one seems to be a code cell. First line of the cell:\n\n{lines}"
+            f"The first cell should be a markdown cell (containing only a title, like `# ...`, and optionally a summary, like `> ...`). This one seems to be a code cell. First line of the cell:\n\n{lines}"
         )
 
     elems = list(direct_children(first.ast))
@@ -244,12 +249,16 @@ def title_heading(cells):
         raise FormattingError(
             message_with_line(
                 first,
-                "The first cell should be just the title for the notebook (like `# ...`) but the title seems to be missing here",
+                "The first cell should be just the title for the notebook (like `# ...`) optionally followed by a summary (like `> ...`), but the title seems to be missing here",
             )
         )
 
     if len(elems) == 1:
         # all good, only element is a title
+        return
+
+    if len(elems) == 2 and title_idx == 0 and is_block_quote(elems[1]):
+        # all good, '# title' followed by '> summary'
         return
 
     # have a title, but there's other things too.
@@ -258,7 +267,7 @@ def title_heading(cells):
     raise FormattingError(
         message_with_line(
             first,
-            "The first cell should contain only the title (like `# ...`) for the notebook. Additional introductory content can be in a separate following cell",
+            "The first cell should contain only the title (like `# ...`) optionally followed by a summary (like `> ...`) for the notebook. Additional introductory content can be in a separate following cell",
             sourcepos=sourcepos,
         )
     )
@@ -334,7 +343,6 @@ def simple_inline_formatting(cells):
     """
     rST doesn't easily supported nested formatting, such as Markdown like [some `code` within a
     link](...) or **`bold code`**, so we disallow it.
-
     http://docutils.sourceforge.net/FAQ.html#is-nested-inline-markup-possible
     """
 
@@ -376,6 +384,43 @@ def simple_inline_formatting(cells):
                     cell,
                     f"Found some nested formatting within a {elem.t} element{summary}, which isn't supported in reStructuredText, as used by Sphinx and Read the Docs. Consider: {'; '.join(suggestions)}",
                     sourcepos=closest_parent_sourcepos(elem),
+                )
+            )
+
+    if errors:
+        raise FormattingError(errors)
+
+
+@checker
+def no_leading_block_quotes(cells):
+    """
+    A block quote at the start of a cell doesn't recieve the necessary separating comment in rST
+
+    A rST quote is just indented text, which can merge with earlier directives unless there's a
+    separating comment. Within a single cell, nbsphinx/nbconvert handles this correctly, but it
+    doesn't when the quote is at the start of the cell. See:
+    - https://github.com/spatialaudio/nbsphinx/issues/450
+    - https://github.com/stellargraph/stellargraph/pull/1398
+    """
+    errors = []
+    for cell in cells:
+        if not isinstance(cell, MarkdownCell):
+            continue
+
+        # unfortunately, the cloud runner cells break this rule (and there doesn't seem to be a good
+        # way to avoid it), so skip them, and we just have to be careful that they get formatted
+        # correctly.
+        if "CloudRunner" in cell.metadata.get("tags", []):
+            continue
+
+        first = cell.ast.first_child
+
+        if is_block_quote(first):
+            errors.append(
+                message_with_line(
+                    cell,
+                    f"Found a block quote (like `> ...`) as the first element of a cell; this must be avoided because it may cause problems during the reStructuredText conversion (https://github.com/spatialaudio/nbsphinx/issues/450). Consider: moving the block quote",
+                    sourcepos=first.sourcepos,
                 )
             )
 
