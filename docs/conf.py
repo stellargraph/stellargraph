95ce3c7c344fdb9d08edfb9d3e4546784a7c0bde
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-05-05 16:25:40 +1000

    Allow the sidebar ToC to be deeper

--- docs/conf.py
+++ docs/conf.py
@@ -117,7 +117,10 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    # allow the sidebar ToC to be arbitrarily deep, good for demos
+    "navigation_depth": -1
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
