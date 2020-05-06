6a3a22dec572aa83dcf1039ed0cb14a735c7dc8f
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-05-06 15:56:25 +1000

    Implement RotatE

--- stellargraph/layer/knowledge_graph.py
+++ stellargraph/layer/knowledge_graph.py
@@ -529,6 +529,208 @@ class DistMult:
     build = deprecated_model_function(in_out_tensors, "build")
 
 
+class RotatEScore(Layer):
+    def __init__(self, margin, **kwargs):
+        super().__init__(**kwargs)
+        self._margin = margin
+
+    def get_config(self):
+        return {**super().get_config(), "margin": self._margin}
+
+    def call(self, inputs):
+        s_re, s_im, r_re, r_im, o_re, o_im = inputs
+        # expansion of s◦r - t
+        re = s_re * r_re - s_im * r_im - o_re
+        im = s_re * r_im + s_im * r_re - o_im
+        # norm the vector: -|| ... ||_2
+        return self._margin - tf.math.sqrt(tf.reduce_sum(re * re + im * im, axis=2))
+
+
+@experimental(reason="demo and documentation is missing")
+class RotatE:
+    """
+    """
+
+    def __init__(
+        self,
+        generator,
+        embedding_dimension,
+        margin=12.0,
+        embeddings_initializer="normal",
+        embeddings_regularizer=None,
+    ):
+        if not isinstance(generator, KGTripleGenerator):
+            raise TypeError(
+                f"generator: expected KGTripleGenerator, found {type(generator).__name__}"
+            )
+
+        graph = generator.G
+        self.num_nodes = graph.number_of_nodes()
+        self.num_edge_types = len(graph._edges.types)
+        self.embedding_dimension = embedding_dimension
+
+        self._scoring = RotatEScore(margin=margin)
+
+        def embed(count):
+            return Embedding(
+                count,
+                embedding_dimension,
+                embeddings_initializer=embeddings_initializer,
+                embeddings_regularizer=embeddings_regularizer,
+            )
+
+        # RotatE generates embeddings in C, which we model as separate real and imaginary embeddings
+        # for node types, and just the phase for edge types (since they have |x| = 1)
+        self._node_embeddings_real = embed(self.num_nodes)
+        self._node_embeddings_imag = embed(self.num_nodes)
+        self._edge_type_embeddings_phase = embed(self.num_edge_types)
+
+    def embeddings(self):
+        node = 1j * self._node_embeddings_imag.embeddings.numpy()
+        node += self._node_embeddings_real.embeddings.numpy()
+
+        phase = self._edge_type_embeddings_phase.embeddings.numpy()
+        rel = 1j * np.cos(phase)
+        rel += np.sin(phase)
+
+        return node, rel
+
+    def rank_edges_against_all_nodes(
+        self, test_data, known_edges_graph, tie_breaking="random"
+    ):
+        """
+        Returns the ranks of the true edges in ``test_data``, when scored against all other similar
+        edges.
+
+        For each input edge ``E = (s, r, o)``, the score of the *modified-object* edge ``(s, r, n)``
+        is computed for every node ``n`` in the graph, and similarly the score of the
+        *modified-subject* edge ``(n, r, o)``.
+
+        This computes "raw" and "filtered" ranks:
+
+        raw
+          The score of each edge is ranked against all of the modified-object and modified-subject
+          ones, for instance, if ``E = ("a", "X", "b")`` has score 3.14, and only one
+          modified-object edge has a higher score (e.g. ``F = ("a", "X", "c")``), then the raw
+          modified-object rank for ``E`` will be 2; if all of the ``(n, "X", "b")`` edges have score
+          less than 3.14, then the raw modified-subject rank for ``E`` will be 1.
+
+        filtered
+          The score of each edge is ranked against only the unknown modified-object and
+          modified-subject edges. An edge is considered known if it is in ``known_edges_graph``
+          which should typically hold every edge in the dataset (that is everything from the train,
+          test and validation sets, if the data has been split). For instance, continuing the raw
+          example, if the higher-scoring edge ``F`` is in the graph, then it will be ignored, giving
+          a filtered modified-object rank for ``E`` of 1. (If ``F`` was not in the graph, the
+          filtered modified-object rank would be 2.)
+
+        Args:
+            test_data: the output of :meth:`KGTripleGenerator.flow` on some test triples
+
+            known_edges_graph (StellarGraph):
+                a graph instance containing all known edges/triples
+
+            tie_breaking ('random', 'top' or 'bottom'):
+                How to rank true edges that tie with modified-object or modified-subject ones, see
+                `Sun et al. "A Re-evaluation of Knowledge Graph Completion Methods"
+                <http://arxiv.org/abs/1911.03903>`_
+
+        Returns:
+            A numpy array of integer raw ranks. It has shape ``N × 2``, where N is the number of
+            test triples in ``test_data``; the first column (``array[:, 0]``) holds the
+            modified-object ranks, and the second (``array[:, 1]``) holds the modified-subject
+            ranks.
+        """
+
+        if not isinstance(test_data, KGTripleSequence):
+            raise TypeError(
+                "test_data: expected KGTripleSequence; found {type(test_data).__name__}"
+            )
+
+        num_nodes = known_edges_graph.number_of_nodes()
+
+        all_node_embs, all_rel_embs = self.embeddings()
+        all_node_embs_conj = all_node_embs.conj()
+
+        raws = []
+        filtereds = []
+
+        # run through the batches and compute the ranks for each one
+        num_tested = 0
+        for ((subjects, rels, objects),) in test_data:
+            num_tested += len(subjects)
+
+            # batch_size x k
+            ss = all_node_embs[subjects, :]
+            rs = all_rel_embs[rels, :]
+            os = all_node_embs[objects, :]
+
+            # reproduce the scoring function for ranking the given subject and relation against all
+            # other nodes (objects), and similarly given relation and object against all
+            # subjects. The bulk operations give speeeeeeeeed.
+            # (num_nodes x k, batch_size x k) -> num_nodes x batch_size
+
+            # (the margin is a fixed offset that doesn't affect relative ranks)
+            mod_o_pred = -np.linalg.norm((ss * rs)[None, :, :] - all_node_embs[:, None, :], axis=2)
+            mod_s_pred = -np.linalg.norm((all_node_embs)[:, None, :] * rs[None, :, :] - os[None, :, :], axis=2)
+
+            mod_o_raw, mod_o_filt = _ranks_from_score_columns(
+                mod_o_pred,
+                true_modified_node_ilocs=objects,
+                unmodified_node_ilocs=subjects,
+                true_rel_ilocs=rels,
+                modified_object=True,
+                known_edges_graph=known_edges_graph,
+                tie_breaking=tie_breaking,
+            )
+            mod_s_raw, mod_s_filt = _ranks_from_score_columns(
+                mod_s_pred,
+                true_modified_node_ilocs=subjects,
+                true_rel_ilocs=rels,
+                modified_object=False,
+                unmodified_node_ilocs=objects,
+                known_edges_graph=known_edges_graph,
+                tie_breaking=tie_breaking,
+            )
+
+            raws.append(np.column_stack((mod_o_raw, mod_s_raw)))
+            filtereds.append(np.column_stack((mod_o_filt, mod_s_filt)))
+
+        # make one big array
+        raw = np.concatenate(raws)
+        filtered = np.concatenate(filtereds)
+        # for each edge, there should be an pair of raw ranks
+        assert raw.shape == filtered.shape == (num_tested, 2)
+
+        return raw, filtered
+
+    def __call__(self, x):
+        s_iloc, r_iloc, o_iloc = x
+
+        s_re = self._node_embeddings_real(s_iloc)
+        s_im = self._node_embeddings_imag(s_iloc)
+
+        r_phase = self._edge_type_embeddings_phase(r_iloc)
+
+        r_re = tf.math.sin(r_phase)
+        r_im = tf.math.cos(r_phase)
+
+        o_re = self._node_embeddings_real(o_iloc)
+        o_im = self._node_embeddings_imag(o_iloc)
+
+        return self._scoring([s_re, s_im, r_re, r_im, o_re, o_im])
+
+    def in_out_tensors(self):
+        s_iloc = Input(shape=1)
+        r_iloc = Input(shape=1)
+        o_iloc = Input(shape=1)
+
+        x_inp = [s_iloc, r_iloc, o_iloc]
+        x_out = self(x_inp)
+
+        return x_inp, x_out
+
+
 def _ranks_from_comparisons(greater, greater_equal, tie_breaking):
     strict = 1 + greater.sum(axis=0)
     # with_ties - strict = the number of elements exactly equal (including the true edge itself)
