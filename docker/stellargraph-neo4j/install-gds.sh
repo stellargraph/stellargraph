12fd744cc9bc684d6e9bb411a641f0bc0d22a82a
kieranricardo <kieranricardo@hotmail.com>
2020-05-29 10:24:26 +1000

    adding gds install

---
+++ docker/stellargraph-neo4j/install-gds.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "--- downloading gds jar for neo4j ${NEO4J_VERSION}"
+
+case "$NEO4J_VERSION" in
+  4.0)
+    gds_version="1.2.1"
+    ;;
+  3.5)
+    gds_version="1.2.1"
+    ;;
+  *)
+    echo "NEO4J_VERSION: unsupported version '${NEO4J_VERSION}'"
+    ;;
+esac
+
+wget --no-verbose "https://github.com/neo4j/graph-data-science/releases/download/${gds_version}/neo4j-graph-data-science-${gds_version}-standalone.jar" --directory-prefix plugins/
+
+# validate the downloaded jar
+# echo "${apoc_sha}  ${PWD}/plugins/apoc-${apoc_version}-all.jar" | sha512sum -c
