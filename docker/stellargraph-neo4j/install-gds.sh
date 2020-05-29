84969de979e13c6792e0bd7383a7f81fe8c9c930
kieranricardo <kieranricardo@hotmail.com>
2020-05-29 12:15:39 +1000

    attempting security fix

--- docker/stellargraph-neo4j/install-gds.sh
+++ docker/stellargraph-neo4j/install-gds.sh
@@ -17,6 +17,7 @@ case "$NEO4J_VERSION" in
 esac
 
 wget --no-verbose "https://github.com/neo4j/graph-data-science/releases/download/${gds_version}/neo4j-graph-data-science-${gds_version}-standalone.jar" --directory-prefix plugins/
+echo 'dbms.security.procedures.unrestricted=apoc.*,gds.*' >> ${PWD}conf/neo4j.conf
 
 # validate the downloaded jar
 # echo "${apoc_sha}  ${PWD}/plugins/apoc-${apoc_version}-all.jar" | sha512sum -c
