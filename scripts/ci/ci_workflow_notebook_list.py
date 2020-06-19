895d171c9c6b700ee5ec8163ae168cfbcebe6100
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-06-19 14:56:38 +1000

    black, header

--- scripts/ci/ci_workflow_notebook_list.py
+++ scripts/ci/ci_workflow_notebook_list.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020 Data61, CSIRO
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import yaml
 import sys
 import glob
@@ -6,9 +22,11 @@ from collections import Counter
 MARKER = "# MARKER: list of all notebooks"
 WORKFLOW = ".github/workflows/ci.yml"
 
+
 def error(message, line):
     print(f"{WORKFLOW}:{line}: error: {message}", file=sys.stderr)
     sys.exit(1)
+
 
 def main():
     with open(WORKFLOW) as f:
@@ -18,7 +36,10 @@ def main():
     try:
         marker_position = contents.index(MARKER)
     except:
-        error(f"failed to find {MARKER!r} comment before the 'notebook:' matrix configuration", 0)
+        error(
+            f"failed to find {MARKER!r} comment before the 'notebook:' matrix configuration",
+            0,
+        )
 
     marker_line = contents.count("\n", marker_position) + 1
 
@@ -33,12 +54,13 @@ def main():
             others = ", ".join(repr(k) for k in obj.keys())
             error(f"expected key {key!r} at {context}, found {others}", marker_line)
 
-
     # check for any notebooks listed more than once
     repeated = [name for name, count in Counter(obj).items() if count > 1]
     if repeated:
         repeated_str = ", ".join(repeated)
-        error(f"found {len(repeated)} notebooks listed twice: {repeated_str}", marker_line)
+        error(
+            f"found {len(repeated)} notebooks listed twice: {repeated_str}", marker_line
+        )
 
     listed = set(obj)
     on_disk = set(glob.glob("demos/**/*.ipynb", recursive=True))
@@ -47,7 +69,9 @@ def main():
         extra = listed - on_disk
         missing = on_disk - listed
 
-        message = [f"found list of {len(listed)} notebooks in notebook testing step to be different to the {len(on_disk)} notebooks on disk"]
+        message = [
+            f"found list of {len(listed)} notebooks in notebook testing step to be different to the {len(on_disk)} notebooks on disk"
+        ]
 
         if extra:
             extra_str = ", ".join(sorted(extra))
@@ -58,7 +82,10 @@ def main():
 
         error("; ".join(message), marker_line)
 
-    print(f"{WORKFLOW}:{marker_line}: success: listed notebooks matches notebooks on disk")
+    print(
+        f"{WORKFLOW}:{marker_line}: success: listed notebooks matches notebooks on disk"
+    )
+
 
 if __name__ == "__main__":
     main()
