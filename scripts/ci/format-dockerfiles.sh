2ac30db1e7db01f8543a77200546922d737c3e24
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-06-24 14:52:36 +1000

    Adjust dockerfile formatting

--- scripts/ci/format-dockerfiles.sh
+++ scripts/ci/format-dockerfiles.sh
@@ -3,15 +3,17 @@
 set -euo pipefail
 
 find . -name Dockerfile -o -name '*.dockerfile' | while IFS= read -r file; do
-  echo "--- Checking formatting of $file"
+  echo "Checking formatting of $file"
   formatted="${file}.formatted"
   dockerfile-utils format --spaces 4 "$file" > "$formatted"
 
   if ! diff "$file" "$formatted"; then
     message="Dockerfile has incorrect formatting according to 'dockerfile-utils format --spaces 4 $file'. Formatted form:
 
-$formatted"
+$(cat $formatted)"
     # replace the newlines with %0A for multiline output
-    echo "::error file=${file}::$message" | awk 1 ORS="%0A"
+    echo -n "::error file=${file}::$message" | awk 1 ORS="%0A"
+    # separate from the next file's output
+    echo
   fi
 done
