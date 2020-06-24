e8ff713d5d8c09d54916bf6d99c0a05832c1449f
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-06-24 15:37:22 +1000

    Fix shellcheck

--- scripts/ci/format-dockerfiles.sh
+++ scripts/ci/format-dockerfiles.sh
@@ -10,7 +10,7 @@ find . -name Dockerfile -o -name '*.dockerfile' | while IFS= read -r file; do
   if ! diff "$file" "$formatted"; then
     message="Dockerfile has incorrect formatting according to 'dockerfile-utils format --spaces 4 $file'. Formatted form:
 
-$(cat $formatted)"
+$(cat "$formatted")"
     # replace the newlines with %0A for multiline output
     echo -n "::error file=${file}::$message" | awk 1 ORS="%0A"
     # separate from the next file's output
