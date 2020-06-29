8503c229ac1df54f6ffe248f54a9ed8c50ae9a35
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-06-29 14:22:45 +1000

    Use a single artifact for all notebooks on CI

--- scripts/ci/run_notebook.sh
+++ scripts/ci/run_notebook.sh
@@ -7,6 +7,3 @@ notebook="$1"
 papermill --execution-timeout=600 \
   --parameters_file "./.buildkite/notebook-parameters.yml" --log-output \
   "$notebook" "$notebook"
-
-notebook_name="$(basename "$notebook")"
-echo "::set-output name=notebook_name::$notebook_name"
